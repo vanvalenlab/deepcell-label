242e0799af4e762cac9163bc6b810884d402b6d5
geneva-miller <geneva.elise.miller@gmail.com>
2019-12-02 10:42:13 -0800

    change cmap to viridis (trk)
    -use color_map.set_bad to keep background black
    -better contrast between background (0) and first cell (1)
    -red highlight still distinct from possible cmap colors

--- browser/caliban.py
+++ browser/caliban.py
@@ -562,8 +562,8 @@ class TrackReview:
 
         self.scale_factor = 2
 
-        self.color_map = plt.get_cmap('cubehelix')
-        self.color_map.set_bad('red')
+        self.color_map = plt.get_cmap('viridis')
+        self.color_map.set_bad('black')
 
         self.current_frame = 0
 
@@ -605,6 +605,7 @@ class TrackReview:
                           cmap="cubehelix")
         else:
             frame = self.tracked[frame][:,:,0]
+            frame = np.ma.masked_equal(frame, 0)
             return pngify(imgarr=frame,
                          vmin=0,
                          vmax=max(self.tracks),
