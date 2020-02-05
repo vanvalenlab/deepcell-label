7f887ccb84c96bfa30687a3bd2e55775bf9a4dc9
geneva-miller <geneva.elise.miller@gmail.com>
2020-02-04 18:39:28 -0800

    add highlighting to pixel-editing mode (CalibanWindow)
    -if highlighting is turned on, labels with the same values that brush is drawing with will be edged in white
      (conv brush value or normal editing value)
    -if conversion brush is turned on, target label will be edged in red to reflect that it is being overwritten
      (especially since brush's eraser state doesn't apply to conversion brush)

--- desktop/caliban.py
+++ desktop/caliban.py
@@ -858,6 +858,14 @@ class CalibanWindow:
             brush_outline = self.generate_ann_boundaries(brush_arr)
         display = self.overlay_RGB(display, brush_outline)
 
+        if self.highlight:
+            white_mask = np.where(self.get_ann_current_frame()[y1:y2,x1:x2] == self.brush.h1, 1, 0)
+            white_mask = self.generate_ann_boundaries(white_mask)
+            display = self.overlay_RGB(display, white_mask)
+            red_mask = np.where(self.get_ann_current_frame()[y1:y2,x1:x2] == self.brush.h2, 1, 0)
+            red_mask = self.generate_ann_boundaries(red_mask, color = 'red')
+            display = self.overlay_RGB(display, red_mask)
+
         comp_img = self.array_to_img(input_array = display.astype(np.uint8),
                                     vmax = None, cmap = None, output = 'pyglet')
 
