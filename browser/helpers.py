d306cb62cba01a7d36e866481b4bf6c4d832ac55
geneva-miller <geneva.elise.miller@gmail.com>
2020-03-10 14:25:19 -0700

    consolidate and clean up helpers.py
    helpers.py does not have any functionality that's different between eb-compatible
    and non-eb-compatible flask apps, so we only need one helpers.py file. Also most
    functionality in helpers.py is never used elsewhere, so it was removed.

--- browser/helpers.py
+++ browser/helpers.py
@@ -1,10 +1,5 @@
 import os
 
-import boto3
-
-from config import S3_KEY, S3_SECRET
-
-
 ALLOWED_EXTENSIONS = set([
     '.txt', '.md', '.markdown', '.pdf',
     '.png', '.jpg', '.jpeg', '.gif',
@@ -39,33 +34,3 @@ def is_npz_file(name):
     return os.path.splitext(str(name).lower())[-1] in {'.npz'}
 
 
-# Connect to the s3 service
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id=S3_KEY,
-    aws_secret_access_key=S3_SECRET
-)
-
-def upload_file_to_s3(file, bucket_name, acl="public-read"):
-
-    """
-    Docs: http://boto3.readthedocs.io/en/latest/guide/s3.html
-    """
-
-    try:
-
-        s3.upload_fileobj(
-            file,
-            bucket_name,
-            file.filename,
-            ExtraArgs={
-                "ACL": acl,
-                "ContentType": file.content_type
-            }
-        )
-
-    except Exception as e:
-        print("Something Happened: ", e)
-        return e
-
-    return "{}{}".format(bucket_name, filename.filename)
