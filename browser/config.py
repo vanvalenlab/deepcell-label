74cb5cf3f4717b7fcbf0d23aa7025a0282d5b56b
William Graf <7930703+willgraf@users.noreply.github.com>
2020-04-02 15:03:06 -0700

    fix whitespace in config.py

--- browser/config.py
+++ browser/config.py
@@ -3,14 +3,14 @@ import sys
 
 from decouple import config
 
-DEBUG 					= True
-PORT 					= 5000
+DEBUG = True
+PORT = config('PORT', cast=int, default=5000)
 
 S3_KEY = config('S3_KEY')
 S3_SECRET = config('S3_SECRET')
 
 MYSQL_USERNAME = config('MYSQL_USERNAME')
 MYSQL_HOSTNAME = config('MYSQL_HOSTNAME')
-MYSQL_PORT = config('MYSQL_PORT', cast = int, default = 3306)
+MYSQL_PORT = config('MYSQL_PORT', cast=int, default=3306)
 MYSQL_PASSWORD = config('MYSQL_PASSWORD')
-MYSQL_DATABASE = config('MYSQL_DATABASE', default = 'caliban')
+MYSQL_DATABASE = config('MYSQL_DATABASE', default='caliban')
