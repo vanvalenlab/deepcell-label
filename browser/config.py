52d558c258c80d71762ac1f36ba55ac988b698ba
William Graf <7930703+willgraf@users.noreply.github.com>
2020-04-07 15:54:17 -0700

    remove MYSQL env vars, use SQLALCHEMY_DATABASE_URI instead.

--- browser/config.py
+++ browser/config.py
@@ -12,12 +12,6 @@ PORT = config('PORT', cast=int, default=5000)
 S3_KEY = config('S3_KEY')
 S3_SECRET = config('S3_SECRET')
 
-MYSQL_USERNAME = config('MYSQL_USERNAME')
-MYSQL_HOSTNAME = config('MYSQL_HOSTNAME')
-MYSQL_PORT = config('MYSQL_PORT', cast=int, default=3306)
-MYSQL_PASSWORD = config('MYSQL_PASSWORD')
-MYSQL_DATABASE = config('MYSQL_DATABASE', default='caliban')
-
 TEMPLATES_AUTO_RELOAD = config('TEMPLATES_AUTO_RELOAD', cast=bool, default=True)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS',
