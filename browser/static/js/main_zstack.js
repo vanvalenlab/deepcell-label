4693510af56748e1ee7eacca618ed22b72328c23
William Graf <7930703+willgraf@users.noreply.github.com>
2020-05-11 14:16:58 -0700

    set *loaded vars to false if they are in the payload. otherwise they stay true.

--- browser/static/js/main_zstack.js
+++ browser/static/js/main_zstack.js
@@ -1296,9 +1296,6 @@ function action(action, info, frame = current_frame) {
         alert(payload.error);
       }
       if (payload.imgs) {
-        rawLoaded = false;
-        segLoaded = false;
-
         // load new value of seg_array
         // array of arrays, contains annotation data for frame
         if (payload.imgs.hasOwnProperty('seg_arr')) {
@@ -1308,15 +1305,11 @@ function action(action, info, frame = current_frame) {
         if (payload.imgs.hasOwnProperty('segmented')) {
           segLoaded = false;
           seg_image.src = payload.imgs.segmented;
-        } else {
-          segLoaded = true;
         }
 
         if (payload.imgs.hasOwnProperty('raw')) {
           rawLoaded = false;
           raw_image.src = payload.imgs.raw;
-        } else {
-          rawLoaded = true;
         }
       }
       if (payload.tracks) {
