ec3114faf7b739d5f7707084222a6d18e9dc1bd1
William Graf <7930703+willgraf@users.noreply.github.com>
2020-05-11 12:13:21 -0700

    use PIL for all image rendering, not just RGB raw.

--- browser/imgutils.py
+++ browser/imgutils.py
@@ -2,19 +2,21 @@
 import io
 
 import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+
 from PIL import Image
 
 
 def pngify(imgarr, vmin, vmax, cmap=None):
     out = io.BytesIO()
-    if cmap is None:
-        img = Image.fromarray(imgarr)
-        img.save(out, format="png")
-    else:
-        plt.imsave(out, imgarr,
-                   vmin=vmin,
-                   vmax=vmax,
-                   cmap=cmap,
-                   format="png")
+
+    if cmap:
+        cmap = plt.get_cmap(cmap)
+        imgarr = Normalize(vmin=vmin, vmax=vmax)(imgarr)
+        # apply the colormap
+        imgarr = cmap(imgarr, bytes=True)
+
+    img = Image.fromarray(imgarr)
+    img.save(out, format="png")
     out.seek(0)
     return out
