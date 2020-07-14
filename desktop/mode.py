056901f23b16fd08836cdc8573a5795f380b729e
geneva-miller <geneva.elise.miller@gmail.com>
2020-07-13 17:07:57 -0700

    text formatting, spacing fixes

--- desktop/mode.py
+++ desktop/mode.py
@@ -75,61 +75,61 @@ class Mode:
 
         elif self.action == "CONVERSION BRUSH VALUE":
             self.text = ("\nClick on the label you want to draw WITH,"
-            " or press N to set the brush to an unused label.")
+                         " or press N to set the brush to an unused label.")
 
     def set_question_single(self):
         if self.action == "FLOOD CELL":
             frame_insert = self.fill_frame_text()
             self.text = ("\nFlood selected region of {} with new label{}?"
-                "\n{}").format(self.label, frame_insert, self.simple_answer)
+                         "\n{}").format(self.label, frame_insert, self.simple_answer)
 
         elif self.action == "TRIM PIXELS":
             frame_insert = self.fill_frame_text()
             self.text = ("\nTrim unconnected pixels away from selected region of label {}{}?"
-                "\n{}").format(self.label, frame_insert, self.simple_answer)
+                         "\n{}").format(self.label, frame_insert, self.simple_answer)
 
         elif self.action == "DELETE":
             frame_insert = self.fill_frame_text()
             self.text = ("\nDelete label {}{}?"
-                "\n{}").format(self.label, frame_insert, self.simple_answer)
+                         "\n{}").format(self.label, frame_insert, self.simple_answer)
 
     def set_question_multiframe_options(self):
         if self.action == "REPLACE":
             self.text = ("\nReplace {} with {}?"
-                "\nSPACE = REPLACE IN ALL FRAMES"
-                 "\nS = REPLACE IN FRAME {} ONLY"
-                 "\nESC = CANCEL").format(self.label_2, self.label_1, self.frame_2)
+                         "\nSPACE = REPLACE IN ALL FRAMES"
+                         "\nS = REPLACE IN FRAME {} ONLY"
+                         "\nESC = CANCEL").format(self.label_2, self.label_1, self.frame_2)
 
         elif self.action == "SWAP":
             if self.frame_1 == self.frame_2:
                 self.text = ("\nSwap {} & {}?"
-                    "\nSPACE = SWAP IN ALL FRAMES"
-                    "\nS = SWAP IN FRAME {} ONLY"
-                    "\nESC = CANCEL").format(self.label_2, self.label_1, self.frame_2)
+                             "\nSPACE = SWAP IN ALL FRAMES"
+                             "\nS = SWAP IN FRAME {} ONLY"
+                             "\nESC = CANCEL").format(self.label_2, self.label_1, self.frame_2)
             else:
                 self.text = ("\nSwap {} & {}?"
-                    "\nSPACE = SWAP IN ALL FRAMES"
-                    "\nESC = CANCEL").format(self.label_2, self.label_1)
+                             "\nSPACE = SWAP IN ALL FRAMES"
+                             "\nESC = CANCEL").format(self.label_2, self.label_1)
 
         elif self.action == "CREATE NEW":
             self.text = ("\nCreate new label from {0} in frame {1}?"
-                "\nSPACE = CREATE IN FRAME {1} AND ALL SUBSEQUENT FRAMES"
-                "\nS = CREATE IN FRAME {1} ONLY"
-                "\nESC = CANCEL").format(self.label, self.frame)
+                         "\nSPACE = CREATE IN FRAME {1} AND ALL SUBSEQUENT FRAMES"
+                         "\nS = CREATE IN FRAME {1} ONLY"
+                         "\nESC = CANCEL").format(self.label, self.frame)
 
         elif self.action == "PREDICT":
             self.text = ("\nPredict 3D relationships between labels?"
-                "\nS = PREDICT THIS FRAME FROM PREVIOUS FRAME"
-                "\nSPACE = PREDICT ALL FRAMES"
-                "\nESC = CANCEL")
+                         "\nS = PREDICT THIS FRAME FROM PREVIOUS FRAME"
+                         "\nSPACE = PREDICT ALL FRAMES"
+                         "\nESC = CANCEL")
 
         elif self.action == "RELABEL":
             self.text = ("\nRelabel annotations?"
-                "\nSPACE = RELABEL IN ALL FRAMES"
-                "\nP = PRESERVE 3D INFO WHILE RELABELING"
-                "\nS = RELABEL THIS FRAME ONLY"
-                "\nU = UNIQUELY RELABEL EACH LABEL"
-                "\nESC = CANCEL")
+                         "\nSPACE = RELABEL IN ALL FRAMES"
+                         "\nP = PRESERVE 3D INFO WHILE RELABELING"
+                         "\nS = RELABEL THIS FRAME ONLY"
+                         "\nU = UNIQUELY RELABEL EACH LABEL"
+                         "\nESC = CANCEL")
 
     def update_prompt(self):
         self.text = ""
@@ -148,20 +148,20 @@ class Mode:
 
             if self.action == "SAVE":
                 self.text = ("\nSave current file?"
-                    "\nSPACE = SAVE"
-                    "\nESC = CANCEL")
+                             "\nSPACE = SAVE"
+                             "\nESC = CANCEL")
 
             elif self.action == "WATERSHED":
                 self.text = ("\nPerform watershed to split {}?"
-                    "\n{}").format(self.label_1, self.simple_answer)
+                             "\n{}").format(self.label_1, self.simple_answer)
 
         elif self.kind == "PROMPT":
             self.set_prompt_text()
 
         elif self.kind == "DRAW":
             self.text = ("\nUsing conversion brush to replace {} with {}."
-                "\nUse ESC to stop using the conversion brush.").format(self.conversion_brush_target,
-                self.conversion_brush_value)
+                         "\nUse ESC to stop using the conversion brush.").format(
+                self.conversion_brush_target, self.conversion_brush_value)
 
         self.update_prompt_additions()
 
@@ -183,20 +183,19 @@ class Mode2D(Mode):
         if self.kind == "QUESTION":
 
             if self.action == "REPLACE":
-                self.text = ("\nReplace {} with {}?\n"
-                        + self.simple_answer).format(self.label_2, self.label_1)
+                self.text = "\nReplace {} with {}?\n{}".format(
+                    self.label_2, self.label_1, self.simple_answer)
 
             elif self.action == "SWAP":
-                self.text = ("\nSwap {} & {}?\n"
-                        + self.simple_answer).format(self.label_1, self.label_2)
+                self.text = "\nSwap {} & {}?\n".format(
+                    self.label_1, self.label_2, self.simple_answer)
 
             elif self.action == "CREATE NEW":
-                self.text = ("\nCreate new label from label {0}?\n"
-                        + self.simple_answer).format(self.label)
+                self.text = "\nCreate new label from label {}?\n{}".format(
+                    self.label, self.simple_answer)
 
             elif self.action == "RELABEL":
-                self.text = ("\nRelabel annotations?\n"
-                        + self.simple_answer)
+                self.text = "\nRelabel annotations?\n{}".format(self.simple_answer)
 
     @staticmethod
     def none():
@@ -219,9 +218,9 @@ class Mode3D(Mode):
         if self.kind == "QUESTION":
             if self.action == "SAVE":
                 self.text = ("\nSave current file?"
-                        "\nSPACE = SAVE"
-                        "\nT = SAVE AS .TRK FILE"
-                        "\nESC = CANCEL")
+                             "\nSPACE = SAVE"
+                             "\nT = SAVE AS .TRK FILE"
+                             "\nESC = CANCEL")
 
     @staticmethod
     def none():
@@ -236,22 +235,23 @@ class ModeTrack(Mode3D):
         if self.kind == "QUESTION":
             if self.action == "SAVE":
                 self.text = ("\nSave current file?"
-                    "\nSPACE = SAVE"
-                    "\nESC = CANCEL")
+                             "\nSPACE = SAVE"
+                             "\nESC = CANCEL")
 
             elif self.action == "PARENT":
-                self.text = "\nMake {} a daughter of {}?\n{}".format(self.label_2, self.label_1, self.simple_answer)
+                self.text = "\nMake {} a daughter of {}?\n{}".format(
+                    self.label_2, self.label_1, self.simple_answer)
 
             elif self.action == "NEW TRACK":
                 self.text = ("\nCreate new track from {0} in frame {1}?"
-                    "\nSPACE = CREATE IN FRAME {1} AND ALL SUBSEQUENT FRAMES"
-                    "\nS = CREATE IN FRAME {1} ONLY"
-                    "\nESC = CANCEL").format(self.label, self.frame)
+                             "\nSPACE = CREATE IN FRAME {1} AND ALL SUBSEQUENT FRAMES"
+                             "\nS = CREATE IN FRAME {1} ONLY"
+                             "\nESC = CANCEL").format(self.label, self.frame)
 
             elif self.action == "REPLACE":
                 self.text = ("\nReplace {} with {}?"
-                     "\nSPACE = REPLACE IN ALL FRAMES"
-                     "\nESC = CANCEL").format(self.label_2, self.label_1)
+                             "\nSPACE = REPLACE IN ALL FRAMES"
+                             "\nESC = CANCEL").format(self.label_2, self.label_1)
 
     @staticmethod
     def none():
