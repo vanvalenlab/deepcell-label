0765e343a101181bc8c64ade84abe9d8cdf0a3d4
geneva-miller <geneva.elise.miller@gmail.com>
2020-07-29 21:26:25 -0700

    send correct args in trk js handle_draw call
    "edit_value" was deprecated in favor of "target_value" and "brush_value"
    to make handle_draw compatible with conversion brush functionality

--- browser/static/js/main_track.js
+++ browser/static/js/main_track.js
@@ -291,10 +291,11 @@ class Mode {
 
   handle_draw() {
     action("handle_draw", {
-      "trace": JSON.stringify(mouse_trace),
-      "edit_value": brush.value,
-      "brush_size": brush.size,
-      "erase": brush.erase,
+      "trace": JSON.stringify(mouse_trace), // stringify array so it doesn't get messed up
+      "target_value": brush.target, // value that we're overwriting
+      "brush_value": brush.value, // we don't update caliban with edit_value, etc each time they change
+      "brush_size": brush.size, // so we need to pass them in as args
+      "erase": (brush.erase && !brush.conv),
       "frame": current_frame
     });
     mouse_trace = [];
