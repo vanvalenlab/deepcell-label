cecd0f5f306a80f8ef27317e023349802de0a5e8
geneva-miller <geneva.elise.miller@gmail.com>
2020-08-03 22:07:04 -0700

    initial commit of zoom/pan handler and cursor classes
    Trying to group mouse/cursor attributes and methods together. However,
    mouse location within image depends on current zoom and offset of
    displayed image, so making zoom and pan methods more modular will likely
    make mouse attributes and methods cleaner as well.

---
+++ browser/static/js/cursor.js
@@ -0,0 +1,45 @@
+// cursor to handle mouse movement and attributes
+class CalibanCursor {
+  constructor() {
+    this.pressed = false;
+  }
+}
+
+// handle updating zooming and panning attributes
+class CanvasView {
+  constructor(width, height) {
+
+    this.width = width;
+    this.height = height;
+    this.sx = 0;
+    this.sy = 0;
+    this.sWidth = 0;
+    this.sHeight = 0;
+    this.zoom = 100;
+    this.zoomLimit = 100;
+
+  }
+
+  pan(dx, dy) {
+    let tempPanX = this.sx - dx;
+    let tempPanY = this.sy - dy;
+
+    // stop panning if at the edge of image (x)
+    if (tempPanX >= 0 && tempPanX + this.sWidth < this.width) {
+      this.sx = tempPanX;
+    } else {
+      tempPanX = Math.max(0, tempPanX);
+      this.sx = Math.min(this.width - this.sWidth, tempPanX);
+    }
+
+    // stop panning if at the edge of image (y)
+    if (tempPanY >= 0 && tempPanY + this.sHeight < this.height) {
+      this.sy = tempPanY;
+    } else {
+      tempPanY = Math.max(0, tempPanY);
+      this.sy = Math.min(this.height - this.sHeight, tempPanY);
+    }
+  }
+
+
+}
