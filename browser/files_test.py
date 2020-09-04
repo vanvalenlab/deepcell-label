5dc0dd788df6ca6c6526e19a55dcc970da5cd1c7
Thomas Dougherty <tdowlingdougherty@gmail.com>
2020-09-04 11:51:13 -0400

    Add test for create_cell_info

--- browser/files_test.py
+++ browser/files_test.py
@@ -20,3 +20,19 @@ def test_init(file_):
     assert len(file_.cell_info) == file_.feature_max
     for feature in range(file_.feature_max):
         assert len(file_.cell_ids[feature]) == len(file_.cell_info[feature])
+
+def test_create_cell_info(file_):
+    for feature in range(file_.feature_max):
+        file_.create_cell_info(feature)
+        labels = file_.annotated[..., feature]
+        labels_uniq = np.unique(labels[labels != 0])
+        assert 0 not in file_.cell_ids[feature]
+        for label in labels_uniq:
+            assert label in file_.cell_ids[feature]
+            assert str(label) == file_.cell_info[feature][label]['label']
+            label_in_frame = np.isin(labels, label).any(axis=(1,2))
+            label_frames = file_.cell_info[feature][label]['frames']
+            no_label_frames = [i for i in range(file_.max_frames) if i not in label_frames]
+            assert label_in_frame[label_frames].all()
+            assert not label_in_frame[no_label_frames].any()
+
