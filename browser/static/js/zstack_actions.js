09da3ab8e477afb81cc0b6bd6bc78693e0664489
Thomas Dougherty <tdowlingdougherty@gmail.com>
2020-10-27 16:56:59 -0700

    Rewrite ChangeFrame/Feature/Channel to not use undo/redo route

--- browser/static/js/zstack_actions.js
+++ browser/static/js/zstack_actions.js
@@ -28,8 +28,8 @@ class ToggleHighlight {
   }
 }
 
+/** Action to change the viewed frame. */
 class ChangeFrame {
-
   constructor(mode, frame) {
     this.mode = mode;
     this.oldValue = current_frame;
@@ -38,37 +38,26 @@ class ChangeFrame {
   }
 
   do() {
-    $.ajax({
-      type: 'POST',
-      url: `${document.location.origin}/changeview/${project_id}/frame/${this.newValue}`,
-      success: handlePayload,
-      async: false
-    });
+    setView('frame', this.newValue);
     current_frame = this.newValue;
-    this.reset_action()
+    if (this.mode.action !== '') { this.mode.clear() };
+    render_info_display();
   }
 
 
   undo() {
-    backendUndo();
+    setView('frame', this.oldValue);
     current_frame = this.oldValue;
-    this.reset_action()
+    if (this.mode.action !== '') { this.mode.clear() };
     render_info_display();
   }
 
   redo() {
-    backendRedo();
-    current_frame = this.newValue;
-    this.reset_action()
-    render_info_display();
-  }
-
-  reset_action() {
-    // actions need to start and end on the same frame
-    if (this.mode.action !== '') { this.mode.clear() };
+    this.do();
   }
 }
 
+/** Action to change the viewed feature. */
 class ChangeFeature {
   constructor(mode, feature) {
     this.mode = mode;
@@ -78,55 +67,47 @@ class ChangeFeature {
   }
 
   do() {
-    $.ajax({
-      type: 'POST',
-      url: `${document.location.origin}/changeview/${project_id}/feature/${this.newValue}`,
-      success: handlePayload,
-      async: false
-    });
+    setView('feature', this.newValue);
     this.mode.feature = this.newValue;
     this.mode.clear();
+    render_info_display();
   }
 
   undo() {
-    backendUndo();
+    setView('feature', this.oldValue);
     this.mode.feature = this.oldValue;
+    this.mode.clear();
+    render_info_display();
   }
 
   redo() {
-    backendRedo();
-    this.mode.feature = this.newValue;
+    this.do();
   }
 }
 
+/** Action to change the viewed channel. */
 class ChangeChannel {
   constructor(mode, adjuster, channel) {
     this.mode = mode;
     this.adjuster = adjuster;
     this.oldValue = mode.channel;
     // channel mode channelMax
-    this.newValue = ((channel % channelMax) + channelMax) % channelMax;
+    this.newValue = ((channel % channel_max) + channel_max) % channel_max;
   }
 
   do() {
-    $.ajax({
-      type: 'POST',
-      url: `${document.location.origin}/changeview/${project_id}/channel/${this.newValue}`,
-      success: handlePayload,
-      async: false
-    });
-    this.mode.clear();
+    setView('channel', this.newValue);
     this.adjust(this.oldValue, this.newValue);
+    // render_info_display();
   }
 
   undo() {
-    backendUndo();
+    setView('channel', this.oldValue);
     this.adjust(this.newValue, this.oldValue);
   }
 
-  reddo() {
-    backendRedo();
-    this.adjust(this.oldValue, this.newValue);
+  redo() {
+    this.do();
   }
 
   adjust(oldValue, newValue) {
@@ -134,11 +115,28 @@ class ChangeChannel {
     adjuster.brightnessMap.set(oldValue, adjuster.brightness);
     adjuster.contrastMap.set(oldValue, adjuster.contrast);
     adjuster.invertMap.set(oldValue, adjuster.displayInvert);
+    this.mode.clear();
     // get brightness/contrast vals for new channel
     adjuster.brightness = adjuster.brightnessMap.get(newValue);
     adjuster.contrast = adjuster.contrastMap.get(newValue);
     adjuster.displayInvert = adjuster.invertMap.get(newValue);
   }
+}
+
+/**
+ * Sends a POST request to change the current frame, feature, or channel.
+ * Handles the image data in the response.
+ * 
+ * @param {string} viewAttr which attribute to change (frame, feature, or channel)
+ * @param {int} value value to set to attribute
+ */
+function setView(viewAttr, value) {
+  $.ajax({
+    type: 'POST',
+    url: `${document.location.origin}/changeview/${project_id}/${viewAttr}/${value}`,
+    success: handlePayload,
+    async: false
+  });
 }
 
 // TODO: adapt this class to only handle label editing actions
