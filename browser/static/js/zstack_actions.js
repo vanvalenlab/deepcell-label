c2fe4c85f3e640f77dda0222827a5e2a460c68d6
Thomas Dougherty <tdowlingdougherty@gmail.com>
2020-10-28 15:33:26 -0700

    Change names with view to display

--- browser/static/js/zstack_actions.js
+++ browser/static/js/zstack_actions.js
@@ -38,7 +38,7 @@ class ChangeFrame {
   }
 
   do() {
-    setView('frame', this.newValue);
+    setDisplay('frame', this.newValue);
     current_frame = this.newValue;
     if (this.mode.action !== '') { this.mode.clear() };
     render_info_display();
@@ -46,7 +46,7 @@ class ChangeFrame {
 
 
   undo() {
-    setView('frame', this.oldValue);
+    setDisplay('frame', this.oldValue);
     current_frame = this.oldValue;
     if (this.mode.action !== '') { this.mode.clear() };
     render_info_display();
@@ -67,14 +67,14 @@ class ChangeFeature {
   }
 
   do() {
-    setView('feature', this.newValue);
+    setDisplay('feature', this.newValue);
     this.mode.feature = this.newValue;
     this.mode.clear();
     render_info_display();
   }
 
   undo() {
-    setView('feature', this.oldValue);
+    setDisplay('feature', this.oldValue);
     this.mode.feature = this.oldValue;
     this.mode.clear();
     render_info_display();
@@ -96,13 +96,13 @@ class ChangeChannel {
   }
 
   do() {
-    setView('channel', this.newValue);
+    setDisplay('channel', this.newValue);
     this.adjust(this.oldValue, this.newValue);
     // render_info_display();
   }
 
   undo() {
-    setView('channel', this.oldValue);
+    setDisplay('channel', this.oldValue);
     this.adjust(this.newValue, this.oldValue);
   }
 
@@ -127,13 +127,13 @@ class ChangeChannel {
  * Sends a POST request to change the current frame, feature, or channel.
  * Handles the image data in the response.
  *
- * @param {string} viewAttr which attribute to change (frame, feature, or channel)
+ * @param {string} displayAttr which attribute to change (e.g. frame, feature, or channel)
  * @param {int} value value to set to attribute
  */
-function setView(viewAttr, value) {
+function setDisplay(displayAttr, value) {
   $.ajax({
     type: 'POST',
-    url: `${document.location.origin}/changedisplay/${project_id}/${viewAttr}/${value}`,
+    url: `${document.location.origin}/changedisplay/${project_id}/${displayAttr}/${value}`,
     success: handlePayload,
     async: false
   });
