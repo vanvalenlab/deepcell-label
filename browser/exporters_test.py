9a21e86406f660d8eed6aa8c27fc0f2b067b6233
tddough98 <40068392+tddough98@users.noreply.github.com>
2020-12-11 15:16:08 -0500

    Add tests for exporters

--- browser/exporters_test.py
+++ browser/exporters_test.py
@@ -1,1 +1,54 @@
 """Tests for exporters.py"""
+
+import pytest
+import io
+
+import models
+import exporters
+from conftest import DummyLoader
+
+
+@pytest.fixture
+def npz_exporter(app, db_session):
+    with app.app_context():
+        db_session.autoflush = False
+        project = models.Project.create(DummyLoader(path='test.npz'))
+        exporter = exporters.Exporter(project)
+        return exporter
+
+
+@pytest.fixture
+def trk_exporter(app, db_session):
+    with app.app_context():
+        db_session.autoflush = False
+        project = models.Project.create(DummyLoader(path='test.trk'))
+        exporter = exporters.Exporter(project)
+        return exporter
+
+
+@pytest.fixture
+def s3_exporter(app, db_session):
+    with app.app_context():
+        db_session.autoflush = False
+        project = models.Project.create(DummyLoader())
+        exporter = exporters.S3Exporter(project)
+        return exporter
+
+
+class TestExporter():
+
+    def test_export_npz(self, npz_exporter):
+        file_ = npz_exporter.export()
+        assert isinstance(file_, io.BytesIO)
+
+    def test_export_trk(self, trk_exporter):
+        file_ = trk_exporter.export()
+        assert isinstance(file_, io.BytesIO)
+
+
+class TestS3Exporter():
+
+    def test_export(self, mocker, s3_exporter):
+        mocked = mocker.patch('boto3.s3.inject.upload_fileobj')
+        s3_exporter.export('test')
+        mocked.assert_called()
