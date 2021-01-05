5582446ffcfec93db9f72015cc9f18714a603ede
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-01-05 14:12:31 -0500

    Clean up and remove comments

--- browser/static/js/main_zstack.js
+++ browser/static/js/main_zstack.js
@@ -1,23 +1,5 @@
-
-var adjuster; 
-
 var controller;
 
-function upload_file(cb) {
-  $.ajax({
-    type: 'POST',
-    url: `${document.location.origin}/api/upload/${outputBucket}/${project_id}`,
-    async: true
-  }).done(cb);
-}
-
 function startDeepCellLabel(settings) {
-  // outputBucket = settings.output_bucket;
-  // rgb = settings.rgb;
-  // display_labels = !settings.rgb;
-  // edit_mode = !settings.label_only;
-
   controller = new Controller(settings.token);
-
-  // getProject(settings.token, rgb, handleFirstPayload);
 }
