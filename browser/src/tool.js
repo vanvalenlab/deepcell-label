826ec070caca2c9d81d8a2cc10c74ae3aa1a976a
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-02-10 13:18:48 -0800

    Update dropdown documentation

--- browser/src/tool.js
+++ browser/src/tool.js
@@ -1,10 +1,26 @@
 import { Controller } from './controller.js';
 import { inspect } from '@xstate/inspect';
+import M from 'materialize-css';
 
 inspect({
   // options
   // url: 'https://statecharts.io/inspect', // (default)
   iframe: false // open in new window
+});
+
+// Initialize Materialize elements
+M.AutoInit();
+document.addEventListener('DOMContentLoaded', function() {
+  // Initialize forms
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems, {});
+  // Initialize collapsibles
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, {
+    accordion: true,
+    inDuration: 400,
+    outDuration: 400,
+  });
 });
 
 export function startDeepCellLabel(settings) {
