c68f55a9e1ffaaf0bb4cac0c2e8dbf217661fd9d
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-02-11 17:48:00 -0800

    Pull out actions and services in backend statechart

--- browser/src/backendMachine.js
+++ browser/src/backendMachine.js
@@ -4,141 +4,150 @@ import $ from 'jquery';
 
 const getModel = () => window.model;
 
-const backendMachine = Machine({
-  id: 'backend',
-  initial: 'idle',
-  context: {
-    data: null,
-    error: null,
-  },
-  states: {
-    idle: {
-      exit: sendParent('LOADING'),
-      on: {
-        EDIT: 'edit',
-        UNDO: 'undo',
-        REDO: 'redo',
-        SETFRAME: 'setFrame',
-        TOGGLERGB: 'toggleRGB',
+const backendMachine = Machine(
+  {
+    id: 'backend',
+    initial: 'idle',
+    context: {
+      data: null,
+      error: null,
+    },
+    states: {
+      idle: {
+        exit: sendParent('LOADING'),
+        on: {
+          EDIT: 'edit',
+          UNDO: 'undo',
+          REDO: 'redo',
+          SETFRAME: 'setFrame',
+          TOGGLERGB: 'toggleRGB',
+        },
       },
-    },
-    edit: {
-      invoke: {
-        id: 'edit',
-        src: (context, event) => $.ajax({
-          type: 'POST',
-          url: `${document.location.origin}/api/edit/${getModel().projectID}/${event.action}`,
-          data: event.args,
-        }),
-        onDone: {
-          target: 'loaded',
-          actions: assign({ data: (_, event) => event.data }),
-        },
-        onError: {
-          target: 'error',
-          actions: assign({ error: (_, event) => event.error }),
-        }
-      }
-    },
-    undo: {
-      invoke: {
-        id: 'undo',
-        src: () => $.ajax({
-          type: 'POST',
-          url: `${document.location.origin}/api/undo/${getModel().projectID}`
-        }),
-        onDone: {
-          target: 'loaded',
-          actions: assign({ data: (_, event) => event.data }),
-        },
-        onError: {
-          target: 'error',
-          actions: assign({ error: (_, event) => event.error }),
+      edit: {
+        invoke: {
+          id: 'edit',
+          src: 'edit',
+          onDone: {
+            target: 'loaded',
+            actions: assign({ data: (_, event) => event.data }),
+          },
+          onError: {
+            target: 'error',
+            actions: assign({ error: (_, event) => event.error }),
+          }
         }
       },
-    },
-    redo: {
-      invoke: {
-        id: 'undo',
-        src: () => $.ajax({
-          type: 'POST',
-          url: `${document.location.origin}/api/redo/${getModel().projectID}`
-        }),
-        onDone: {
-          target: 'loaded',
-          actions: assign({ data: (_, event) => event.data }),
+      undo: {
+        invoke: {
+          id: 'undo',
+          src: 'undo',
+          onDone: {
+            target: 'loaded',
+            actions: assign({ data: (_, event) => event.data }),
+          },
+          onError: {
+            target: 'error',
+            actions: assign({ error: (_, event) => event.error }),
+          }
         },
-        onError: {
-          target: 'error',
-          actions: assign({ error: (_, event) => event.error }),
+      },
+      redo: {
+        invoke: {
+          id: 'redo',
+          src: 'redo',
+          onDone: {
+            target: 'loaded',
+            actions: assign({ data: (_, event) => event.data }),
+          },
+          onError: {
+            target: 'error',
+            actions: assign({ error: (_, event) => event.error }),
+          }
+        },
+      },
+      setFrame: {
+        invoke: {
+          id: 'setFrame',
+          src: 'setFrame',
+          onDone: {
+            target: 'loaded',
+            actions: assign({ data: (_, event) => event.data }),
+          },
+          onError: {
+            target: 'error',
+            actions: assign({ error: (_, event) => event.error }),
+          }
+        },
+      },
+      toggleRGB: {
+        invoke: {
+          id: 'toggleRGB',
+          src: 'toggleRGB',
+          onDone: {
+            target: 'loaded',
+            actions: assign({ data: (_, event) => event.data }),
+          },
+          onError: {
+            target: 'error',
+            actions: assign({ error: (_, event) => event.error }),
+          }
+        },
+      },
+      loaded: {
+        on: {
+          '': {
+            target: 'idle',
+            actions: ['handlePayload', sendParent('LOADED')]
+          }
         }
       },
-    },
-    setFrame: {
-      invoke: {
-        id: 'setFrame',
-        src: (context, event) => {
-          const promise = $.ajax({
-            type: 'POST',
-            url: `${document.location.origin}/api/changedisplay/${getModel().projectID}/${event.dimension}/${event.value}`,
-          });
-          promise.then(() => { getModel()[event.dimension] = event.value });
-          return promise;
-        },
-        onDone: {
-          target: 'loaded',
-          actions: assign({ data: (_, event) => event.data }),
-        },
-        onError: {
-          target: 'error',
-          actions: assign({ error: (_, event) => event.error }),
-        }
-      },
-    },
-    toggleRGB: {
-      invoke: {
-        id: 'toggleRGB',
-        src: (context, event) => {
-          const promise = $.ajax({
-            type: 'POST',
-            url: `${document.location.origin}/api/rgb/${getModel().projectID}/${!getModel().rgb}`,
-          });
-          promise.then(() => { getModel().rgb = !getModel().rgb });
-          return promise;
-        },
-        onDone: {
-          target: 'loaded',
-          actions: assign({ data: (_, event) => event.data }),
-        },
-        onError: {
-          target: 'error',
-          actions: assign({ error: (_, event) => event.error }),
-        }
-      },
-    },
-    loaded: {
-      on: {
-        '': {
-          target: 'idle',
-          actions: [
-            (context) => getModel().handlePayload(context.data),
-            sendParent('LOADED'),
-          ]
-        }
-      }
-    },
-    error: {
-      on: {
-        '': {
-          target: 'idle',
-          actions: [
-            (context) => console.log(context.error),
-            sendParent('ERROR'),
-          ]
+      error: {
+        on: {
+          '': {
+            target: 'idle',
+            actions: ['logError', sendParent('ERROR')]
+          }
         }
       }
     }
+  },
+  {
+    actions: {
+      logError: (context) => console.log(context.error),
+      handlePayload: (context) => getModel().handlePayload(context.data),
+    },
+    services: {
+      edit: (context, event) => $.ajax({
+        type: 'POST',
+        url: `${document.location.origin}/api/edit/${getModel().projectID}/${event.action}`,
+        data: event.args,
+      }),
+      undo: () => $.ajax({
+        type: 'POST',
+        url: `${document.location.origin}/api/undo/${getModel().projectID}`
+      }),
+      redo: () => $.ajax({
+        type: 'POST',
+        url: `${document.location.origin}/api/redo/${getModel().projectID}`
+      }),
+      setFrame: (context, event) => {
+        const promise = $.ajax({
+          type: 'POST',
+          url: `${document.location.origin}/api/changedisplay/${getModel().projectID}/${event.dimension}/${event.value}`,
+        });
+        promise.then(() => { getModel()[event.dimension] = event.value });
+        return promise;
+      },
+      toggleRGB: (context, event) => {
+        const promise = $.ajax({
+          type: 'POST',
+          url: `${document.location.origin}/api/rgb/${getModel().projectID}/${!getModel().rgb}`,
+        });
+        promise.then(() => { getModel().rgb = !getModel().rgb });
+        return promise;
+      },
+    }
   }
-});
+);
 
 export default backendMachine;
