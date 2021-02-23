63c4f942131ae93c332a95e50898309a3a7ded97
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-02-22 16:22:22 -0800

    "Mock up control panel rows Slice, Label Display, and Raw Display"

--- src/ControlPanel.js
+++ src/ControlPanel.js
@@ -10,6 +10,18 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+import Button from '@material-ui/core/Button';
+import CheckIcon from '@material-ui/icons/Check';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+
+
 
 import { useService } from '@xstate/react';
 import { labelService } from './service';
@@ -59,17 +71,164 @@ function ControlRow(props) {
   )
 }
 
+function SliceSlider(props) {
+  const { value, max, handler } = props;
+
+  return (
+    <>
+      <Slider
+        value={value}
+        valueLabelDisplay="auto"
+        step={1}
+        marks
+        min={0}
+        max={max}
+        onChange={handler}
+      />
+    </>
+  )
+}
+
+
+function SliceRow() {
+  const classes = useRowStyles();
+  const [current, send] = useService(labelService);
+
+  const handleFrameChange = (event, newValue) => {
+    // send({ type: 'SETFRAME', value: newValue });
+  };
+
+  const handleChannelChange = (event, newValue) => {
+    // send({ type: 'SETFRAME', value: newValue });
+  };
+
+  const handleFeatureChange = (event, newValue) => {
+    // send({ type: 'SETFRAME', value: newValue });
+  };
+
+  return (
+    <ControlRow name={"Slice"} header={"Frame: 0"}>
+      <Typography id="discrete-slider" gutterBottom>
+        Frame
+      </Typography>
+      <SliceSlider value={current.frame} max={current.numFrames - 1 || 25} handler={handleFrameChange}/>
+      <Typography gutterBottom>
+        Channel
+      </Typography>
+      <SliceSlider value={current.channel} max={current.numChannels - 1 || 2} handler={handleChannelChange}/>
+      <Typography gutterBottom>
+        Feature
+      </Typography>
+      <SliceSlider value={current.feature} max={current.numFeatures - 1 || 1} handler={handleFeatureChange}/>
+    </ControlRow>
+  )
+}
+
+function RawDisplayRow() {
+  const [current, send] = useService(labelService);
+
+  const handleBrightnessChange = (event, newValue) => {
+    // send({ type: 'SETCONTRAST', value: newValue });
+  };
+
+  const handleContrastChange = (event, newValue) => {
+    // send({ type: 'SETCONTRAST', value: newValue });
+  };
+
+  const handleInvertChange = (event, newValue) => {
+    // send({ type: 'SETINVERT', value: newValue });
+  };
+
+  return (
+    <ControlRow name={"Raw Display"} header={"brightness: 0"}>
+      <Typography gutterBottom>
+        Brightness
+      </Typography>
+      <Slider
+        value={current.brightness}
+        valueLabelDisplay="auto"
+        min={0}
+        max={1}
+        onChange={handleBrightnessChange}
+      />
+      <Typography gutterBottom>
+        Contrast
+      </Typography>
+      <Slider
+        value={current.contrast}
+        valueLabelDisplay="auto"
+        min={0}
+        max={1}
+        onChange={handleContrastChange}
+      />
+      <ToggleButton
+        value="check"
+        selected={current.invert}
+        onChange={handleInvertChange}
+      >
+        <CheckIcon />
+      </ToggleButton>
+    </ControlRow>
+  );
+
+
+}
+
+function LabelDisplayRow() {
+  const [current, send] = useService(labelService);
+
+  const handleHighlightChange = () => {
+    // send({ type: 'SETHIGHLIGHT', value: !current.highlight });
+  };
+
+  const handleOutlineChange = (event, newValue) => {
+    // send({ type: 'SETOUTLINE', value: newValue });
+  };
+
+  const handleOpacityChange = (event, newValue) => {
+    // send({ type: 'SETOPACITY', value: newValue });
+  };
+
+  return (
+    <ControlRow name={"Label Display"} header={"highlight: on"}>
+      <ToggleButton
+        value="check"
+        selected={current.highlight}
+        onChange={handleHighlightChange}
+      >
+        <CheckIcon />
+      </ToggleButton>
+      <FormControl component="fieldset">
+        <FormLabel component="legend">Outline</FormLabel>
+        <RadioGroup row aria-label="outline" name="outline" value={current.outline || "selected"} onChange={handleOutlineChange}>
+          <FormControlLabel value="all" control={<Radio />} label="All" />
+          <FormControlLabel value="selected" control={<Radio />} label="Selected" />
+          <FormControlLabel value="none" control={<Radio />} label="None" />
+        </RadioGroup>
+      </FormControl>
+      <Typography gutterBottom>
+        Opacity
+      </Typography>
+      <Slider
+        value={current.opacity}
+        valueLabelDisplay="auto"
+        min={0}
+        max={1}
+        onChange={handleOpacityChange}
+      />
+    </ControlRow>
+  );
+}
+
 export default function ControlPanel() {
+
   return (
     <TableContainer id='control-panel' component={Paper}>
       <Table aria-label="collapsible table">
         <TableBody>
-          <ControlRow name={"Slice"} header={"Frame: 0"}>
-            {"Frame Slider"}
-          </ControlRow>
-          <ControlRow name={"Display"} header={"highlight: on"}>
-            {"Highlight button"}
-          </ControlRow>
+          <SliceRow />
+          <LabelDisplayRow />
+          <RawDisplayRow />
           <ControlRow name={"Label"} header={"label: 1"}/>
           <ControlRow name={"Tool"} header={"brush"}/>
         </TableBody>
