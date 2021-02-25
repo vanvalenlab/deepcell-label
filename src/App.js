8ae71a3f3ad687e31d923b6c80560f726a1f759f
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-02-25 09:59:32 -0800

    Add Footer component and use Box instead of Grid

--- src/App.js
+++ src/App.js
@@ -1,46 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from "@material-ui/core/styles";
 import ControlPanel from './ControlPanel';
 import Navbar from './Navbar';
 import Canvas from './Canvas';
 import InstructionPane from './InstructionPane';
-// import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
+import Footer from './Footer';
 
 function App() {
+
+  const styles = makeStyles({
+    root: {
+      textAlign: 'center',
+      height: '100vh',
+    },
+    controlPanel: {
+      flex: '0 1 auto',
+    },
+    // canvas: {
+    //   flex: '1 1 auto',
+    //   height: '90vh'
+    // }
+  });
+
   return (
-    <div className="App">
+    <div className={styles.root}>
       <Navbar />
       <InstructionPane />
-      <Grid container spacing={1}>
-        <Grid item xs={3}>
-          <ControlPanel />
-        </Grid>
-        <Grid item xs id='canvas-grid-item'>
+      <Box display="flex" justifyContent="space-evenly" alignItems="flex-start">
+        <Box boxShadow={3}>
+          <ControlPanel className={styles.controlPanel}q/>
+        </Box>
+        <Box display="flex" boxShadow={10}>
           <Canvas />
-        </Grid>
-      </Grid>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <footer className="page-footer white">
-        <div className="footer-copyright white">
-          <div className="container center-align grey-text darken-5-text">
-          © 2016-2021 The Van Valen Lab at the California Institute of Technology (Caltech). All rights reserved.
-          </div>
-        </div>
-      </footer>
+        </Box>
+      </Box>
+      <Footer />
     </div>
   );
 }
