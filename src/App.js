801ca684c0fbeeb0bf130197c0d4435b2246ccfb
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-03-03 13:12:25 -0800

    Size canvas with object-fit: contain

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import './App.css';
 import Box from '@material-ui/core/Box';
 import { makeStyles } from "@material-ui/core/styles";
-import ControlPanel from './ControlPanel';
+import ControlPanel from './ControlPanel/ControlPanel';
 import Navbar from './Navbar';
-import Canvas from './Canvas';
-import { RawCanvas, LabelCanvas, OutlineCanvas } from './Canvas';
+import Canvas from './Canvas/Canvas';
+import { EmptyCanvas, RawCanvas, LabelCanvas, OutlineCanvas } from './Canvas/Canvas';
 import InstructionPane from './InstructionPane';
-import Footer from './Footer';
+import Footer from './Footer/Footer';
+
 
 const useStyles = makeStyles({
   root: {
+    boxSizing: 'border-box',
     display: 'flex',
     minHeight: '100vh',
     flexDirection: 'column',
   },
   main: {
-    display: "flex",
+    display: "inline-flex",
     flexGrow: 1,
     padding: 40,
-    alignItems: "flex-start",
+    alignItems: "stretch",
     justifyContent: "space-evenly",
+    height: 'calc(100vh - 66px - 57px - 60px - 80px - 1px)'
   },
   controlPanel: {
-    flex: '0 1 auto',
+    flex: '0 0 auto',
   },
   canvas: {
+    position: 'relative',
     flex: '1 1 auto',
-    // height: '90vh'
-  }
+    // height: 0,
+    // paddingTop: 'calc(591.44 / 1127.34 * 100%)',
+  },
 });
 
 function App() {
@@ -38,10 +43,11 @@ function App() {
       <Navbar />
       <InstructionPane />
       <Box className={styles.main}>
-        <Box boxShadow={3}>
+        <Box>
           <ControlPanel className={styles.controlPanel}/>
         </Box>
-        <Box display="flex" className={styles.canvas} boxShadow={10}>
+        <Box className={styles.canvas} boxShadow={10}>
+          <EmptyCanvas />
           <RawCanvas />
           <LabelCanvas />
           <OutlineCanvas />
---
+++ src/Canvas/Canvas.js
@@ -0,0 +1,295 @@
+import React, { useState, useEffect } from 'react';
+import { useService } from '@xstate/react';
+import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import useCanvas from '../useCanvas';
+import { labelService } from '../statechart/service';
+
+
+const useStyles = makeStyles(() => {
+  const [current, send] = useService(labelService);
+  // const { sx, sy, sWidth, sHeight, width, height } = current.context;
+  const [sx, sy, sWidth, sHeight, width, height] = [0, 0, 4* 160, 4* 160, 4 * 160, 4 * 160];
+
+  const padding = 5;
+  const topColor = (Math.floor(sy) === 0) ? 'white' : 'black';
+  const bottomColor = 'green'; // (Math.ceil(sy + sHeight) === height) ? 'white' : 'black';
+  const rightColor = 'red'; //(Math.ceil(sx + sWidth) === width) ? 'white' : 'black';
+  const leftColor = (Math.floor(sx) === 0) ? 'white' : 'black';
+
+  return {
+    canvas: {
+      boxSizing: 'border-box',
+      borderTop: `${padding}px solid ${topColor}`,
+      borderBottom: `${padding}px solid ${bottomColor}`,
+      borderLeft: `${padding}px solid ${leftColor}`,
+      borderRight: `${padding}px solid ${rightColor}`,
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      // height: 4 * 160,
+      // width: 4 * 160,
+      imageRendering: 'pixelated',
+      objectFit: 'contain',
+      maxHeight: '100%',
+      // maxWidth: '100%',
+      // height: '100%',
+      width: '100%',
+      objectPosition: 'top left',
+      // imageRendering: '-moz-crisp-edges',
+      // imageRendering: 'crisp-edges',
+      // imageRendering: 'optimizeSpeed',             // Older versions of FF
+      // imageRendering: '-moz-crisp-edges',          // FF 6.0+
+      // imageRendering: '-webkit-optimize-contrast', // Webkit (non standard naming)
+      // imageRendering: '-o-crisp-edges',            // OS X & Windows Opera (12.02+)
+      // imageRendering: 'crisp-edges',               // Possible future browsers.
+      // msInterpolationMode: 'nearest-neighbor',
+    },
+    empty: {
+      boxSizing: 'border-box',
+      objectFit: 'contain',
+      maxHeight: '100%',
+      // maxWidth: '100%',
+      // height: '100%',
+      width: '100%',
+      objectPosition: 'top left',
+      borderTop: `${padding}px solid ${topColor}`,
+      borderBottom: `${padding}px solid ${bottomColor}`,
+      borderLeft: `${padding}px solid ${leftColor}`,
+      borderRight: `${padding}px solid ${rightColor}`,
+    },
+  };
+});
+
+const width = 160;
+const height = 160;
+const padding = 5;
+
+
+const Canvas = props => {
+  const styles = useStyles();
+  const [current, send] = useService(labelService);
+  // const { sx, sy, sWidth, sHeight } = current.state.context;
+  const [sx, sy, sWidth, sHeight] = [0, 0, 160, 160];
+  
+  // const [scale, scaledHeight, scaledWidth] = useScaledDimensions(height, width, padding);
+  const [scale, scaledHeight, scaledWidth] = [4, 4 * 160, 4 * 160];
+  
+  // const [raw, label, labelArray] = current.state.context;
+  const [data, setData] = useState(null);
+  const [raw, setRaw] = useState(new Image());
+  const [label, setLabel] = useState(new Image());
+  const [labelArray, setLabelArray] = useState([[]]);
+
+  const draw = (ctx) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.drawImage(
+      raw,
+      sx, sy,
+      sWidth, sHeight,
+      0, 0,
+      scaledWidth,
+      scaledHeight
+    );
+  };
+  
+  const canvasRef = useCanvas(draw);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'http://0.0.0.0:5000/api/project/qUtuBnLdw5S-',
+      );
+      console.log(result.data);
+      setData(result.data);
+      const rawImage = new Image();
+      rawImage.src = result.data.imgs.raw;
+      setRaw(rawImage);
+      const labelImage = new Image();
+      labelImage.src = result.data.imgs.segmented;
+      setLabel(labelImage);
+    };
+ 
+    fetchData();
+  }, []);
+  
+  return <canvas id='canvas'
+    className={styles.canvas}
+    ref={canvasRef}
+    width={scaledWidth}
+    height={scaledHeight}
+    {...props}
+  />;
+};
+
+export default Canvas;
+
+export const EmptyCanvas = props => {
+  const styles = useStyles();
+  const [scale, scaledHeight, scaledWidth] = [4, 160, 160];
+
+  return <canvas id='canvas'
+    className={styles.empty}
+    width={scaledWidth}
+    height={scaledHeight}
+    {...props}
+  />;
+}
+
+export const RawCanvas = props => { 
+  const styles = useStyles();
+  const [current, send] = useService(labelService);
+  const [raw, setRaw] = useState(new Image());
+  const [sx, sy, sWidth, sHeight] = [0, 0, 160, 160];
+  const [scale, scaledHeight, scaledWidth] = [4, 160, 160];
+
+  const draw = (ctx) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.drawImage(
+      raw,
+      sx, sy,
+      sWidth, sHeight,
+      0, 0,
+      scaledWidth,
+      scaledHeight
+    );
+  };
+
+  const canvasRef = useCanvas(draw);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios('http://0.0.0.0:5000/api/project/qUtuBnLdw5S-');
+      const rawImage = new Image();
+      rawImage.src = result.data.imgs.raw;
+      setRaw(rawImage);
+    };
+ 
+    fetchData();
+  }, []);
+
+  return <canvas id='raw-canvas'
+    className={styles.canvas}
+    ref={canvasRef}
+    width={scaledWidth}
+    height={scaledHeight}
+    {...props}
+  />;
+};
+
+export const LabelCanvas = props => { 
+  const styles = useStyles();
+  const [current, send] = useService(labelService);
+  const [label, setLabel] = useState(new Image());
+  const [sx, sy, sWidth, sHeight] = [0, 0, 160, 160];
+  const [scale, scaledHeight, scaledWidth] = [4, 160, 160];
+
+  const draw = (ctx) => {
+    ctx.save();
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.globalAlpha = 0.5;
+    ctx.drawImage(
+      label,
+      sx, sy,
+      sWidth, sHeight,
+      0, 0,
+      scaledWidth,
+      scaledHeight
+    );
+    ctx.restore();
+  };
+
+  const canvasRef = useCanvas(draw);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios('http://0.0.0.0:5000/api/project/qUtuBnLdw5S-');
+      const labelImage = new Image();
+      labelImage.src = result.data.imgs.segmented;
+      setLabel(labelImage);
+    };
+    fetchData();
+  }, []);
+
+  return <canvas id='label-canvas'
+    className={styles.canvas}
+    ref={canvasRef}
+    width={scaledWidth}
+    height={scaledHeight}
+    {...props}
+  />;
+};
+
+export const OutlineCanvas = props => {
+  const styles = useStyles();
+  const [current, send] = useService(labelService);
+  const [labelArray, setLabelArray] = useState([[0]]);
+  const [sx, sy, sWidth, sHeight] = [0, 0, 160, 160];
+  const [scale, scaledHeight, scaledWidth] = [4, 160, 160];
+  const [width, height] = [160, 160];
+  const [foreground, background] = [1, 2];
+  const [all, setAll] = useState(false);
+
+  const outlineCanvas = new OffscreenCanvas(width, height);
+  const outlineCtx = outlineCanvas.getContext('2d');
+
+  const draw = (ctx) => {
+    ctx.save();
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.drawImage(
+      outlineCanvas,
+      sx, sy,
+      sWidth, sHeight,
+      0, 0,
+      scaledWidth,
+      scaledHeight
+    );
+    ctx.restore();
+  };
+
+  const canvasRef = useCanvas(draw);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios('http://0.0.0.0:5000/api/project/qUtuBnLdw5S-');
+      setLabelArray(result.data.imgs.seg_arr);
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const width = labelArray[0].length;
+    const height = labelArray.length;
+    const array = new Uint8ClampedArray(width * height * 4);
+    // use label array to figure out which pixels to recolor
+    for (let j = 0; j < height; j += 1) { // y
+      for (let i = 0; i < width; i += 1) { // x
+        const label = labelArray[j][i];
+        if (label === -1 * foreground) {
+          array[(j * width  + i) * 4 + 0] = 255;
+          array[(j * width  + i) * 4 + 1] = 255;
+          array[(j * width  + i) * 4 + 2] = 255;
+          array[(j * width  + i) * 4 + 3] = 255;
+        } else if (label === -1 * background) {
+          array[(j * width  + i) * 4 + 0] = 255;
+          array[(j * width  + i) * 4 + 1] = 0;
+          array[(j * width  + i) * 4 + 2] = 0;
+          array[(j * width  + i) * 4 + 3] = 255;
+        }
+      }
+    }
+    const data = new ImageData(array, width, height);
+    outlineCtx.putImageData(data, 0, 0);
+  }, [labelArray, foreground, background, outlineCtx]);
+
+  return <canvas id='outline-canvas'
+    className={styles.canvas}
+    ref={canvasRef}
+    width={scaledWidth}
+    height={scaledHeight}
+    {...props}
+  />;
+};
+
+export const BrushCanvas = () => { };
