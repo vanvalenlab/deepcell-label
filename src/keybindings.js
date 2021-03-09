3acc3de79f80922be15127eb0ce7406bb2ccaae1
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-03-08 23:05:50 -0800

    Handle keybinds with Mousetrap

--- src/keybindings.js
+++ src/keybindings.js
@@ -1,28 +1,5 @@
 import { canvasService } from './statechart/service';
+import { bind } from 'mousetrap';
 
-
-const canvasKeydownLookup = {
-  ' ': 'keydown.Space',
-};
-
-const canvasKeyupLookup = {
-  ' ': 'keyup.Space',
-};
-
-window.addEventListener('keydown', (e) => handleKeydown(e));
-window.addEventListener('keyup', (e) => handleKeyup(e));
-
-
-function handleKeydown(event) {
-  if (canvasKeydownLookup[event.key]) {
-    canvasService.send(canvasKeydownLookup[event.key]);
-    console.log('space DOWN sent to canvas')
-  }
-}
-
-function handleKeyup(event) {
-  if (canvasKeyupLookup[event.key]) {
-    canvasService.send(canvasKeyupLookup[event.key]);
-    console.log('space UP sent to canvas')
-  }
-}
+bind('space', () => canvasService.send('keydown.Space'));
+bind('space', () => canvasService.send('keyup.Space'), 'keyup');
