bbe06d6518b2a63f9033faff3ce6edd09b4612fb
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-03-25 12:36:24 -0700

    Send events to channelActor from ChannelControls

--- src/ControlPanel/ChannelControls.js
+++ src/ControlPanel/ChannelControls.js
@@ -1,37 +1,17 @@
 import React from 'react';
-import { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
-import Button from '@material-ui/core/Button';
-import CheckIcon from '@material-ui/icons/Check';
 import ToggleButton from '@material-ui/lab/ToggleButton';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
-import Box from '@material-ui/core/Box';
+import { useActor } from '@xstate/react';
 
-
-import { useService } from '@xstate/react';
-import { rawAdjustService } from '../statechart/service';
 import ControlRow from './ControlRow';
+import { useRaw } from '../ServiceContext';
 
-export default function RawDisplayRow() {
-  const [current, send] = useService(rawAdjustService);
+export default function ChannelControls() {
+  const [currentRaw, sendRaw] = useRaw();
+  const [current, send] = useActor(currentRaw.context.channelActor);
 
-  const { invert, grayscale, brightness, contrast } = current.context; 
+  const { invert, grayscale, brightness, contrast } = current.context;
 
   const handleBrightnessChange = (event, newValue) => {
     send({ type: 'SETBRIGHTNESS', brightness: newValue });
@@ -72,7 +52,6 @@ export default function RawDisplayRow() {
       </Typography>
       <Slider
         value={brightness}
-        // defaultValue={current.brightness}
         valueLabelDisplay="auto"
         min={-1}
         max={1}
@@ -85,7 +64,6 @@ export default function RawDisplayRow() {
       </Typography>
       <Slider
         value={contrast}
-        // defaultValue={current.contrast}
         valueLabelDisplay="auto"
         min={-1}
         max={1}
@@ -95,6 +73,4 @@ export default function RawDisplayRow() {
       />
     </ControlRow>
   );
-
-
 }
