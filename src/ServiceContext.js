c096b17ee4e0a7efea1b972a48e91c8d94f94a01
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-03-25 12:38:35 -0700

    Provide hooks and Context to use actors in ServiceContext

--- src/ServiceContext.js
+++ src/ServiceContext.js
@@ -1,20 +1,146 @@
-import React, { createContext } from 'react';
-import { useInterpret } from '@xstate/react';
+import React, { createContext, useContext, useEffect } from 'react';
+import { useInterpret, useActor } from '@xstate/react';
 import { useLocation } from "react-router-dom";
 import createFrameMachine from './statechart/frameMachine';
+import createDeepcellLabelMachine from './statechart/deepcellLabelMachine';
+import { bind, unbind } from 'mousetrap';
 
-export const FrameContext = createContext();
+export const LabelContext = createContext();
+
+
+export const useLabelService = () => {
+  return {
+    service: useReturnContext(LabelContext),
+  };
+};
+
+function useReturnContext(contextType) {
+    const context = useContext(contextType);
+    if (context === undefined) {
+        throw new Error(`${contextType} must be used within its appropriate parent provider`);
+    }
+    return context;
+}
+
+export function useLabel() {
+  const { service } = useLabelService();
+  const [state, send] = useActor(service);
+  return [state, send];
+}
+
+export function useImage() {
+  const { service } = useLabelService();
+  const { image } = service.state.children;
+  const [state, send] = useActor(image);
+
+  useEffect(() => {
+    bind('a', () => {
+      const { frame, numFrames } = state.context;
+      console.log((frame - 1 + numFrames) % numFrames);
+      send({ type: 'SETFRAME', frame: (frame - 1 + numFrames) % numFrames });
+    });
+    bind('d', () => {
+      const { frame, numFrames } = state.context;
+      send({ type: 'SETFRAME', frame: (frame + 1) % numFrames });
+    });
+    bind('shift+c', () => {
+      const { channel, numChannels } = state.context;
+      send({ type: 'SETCHANNEL', channel: (channel - 1 + numChannels) % numChannels });
+    });
+    bind('c', () => {
+      const { channel, numChannels } = state.context;
+      send({ type: 'SETCHANNEL', channel: (channel + 1) % numChannels });
+    });
+    bind('shift+f', () => {
+      const { feature, numFeatures } = state.context;
+      send({ type: 'SETFEATURE', feature: (feature - 1 + numFeatures) % numFeatures });
+    });
+    bind('f', () => {
+      const { feature, numFeatures } = state.context;
+      send({ type: 'SETFEATURE', feature: (feature + 1) % numFeatures });
+    });
+
+    return () => {
+      unbind('a');
+      unbind('d');
+      unbind('c');
+      unbind('shift+c');
+      unbind('f')
+      unbind('shift+f');
+    }
+  }, [state, send]);
+
+  return [state, send];
+}
+
+export function useRaw() {
+  const { service } = useLabelService();
+  const { image } = service.state.children;
+  const { raw } = image.state.children;
+  const [state, send] = useActor(raw);
+  return [state, send];
+}
+
+// Doesn't work because changing channelActor does not update the hook
+// export function useChannel() {
+//   const { service } = useLabelService();
+//   const { frame } = service.state.children;
+//   const { raw } = frame.state.children;
+//   const { channelActor } = raw.state.context;
+//   const [state, send] = useActor(channelActor);
+//   return [state, send];
+// }
+
+// Doesn't work because changing featureActor does not update the hook
+// export function useFeature() {
+//   const { service } = useLabelService();
+//   const { label } = service.state.children;
+//   const { featureActor } = label.state.context;
+//   const [state, send] = useActor(featureActor);
+//   return [state, send];
+// }
+
+export function useLabeled() {
+  const { service } = useLabelService();
+  const { image } = service.state.children;
+  const { labeled } = image.state.children;
+  const [state, send] = useActor(labeled);
+  return [state, send];
+}
+
+export function useCanvas() {
+  const { service } = useLabelService();
+  const { canvas } = service.state.children;
+  const [state, send] = useActor(canvas);
+  useEffect(() => {
+    bind('space', (event) => {
+      if (event.repeat) return;
+      send('keydown.Space');
+    });
+    bind('space', () => send('keyup.Space'), 'keyup');
+    return () => {
+      unbind('space');
+      unbind('space', 'keyup');
+    }
+  }, []);
+  return [state, send];
+}
 
 const ServiceContext = (props) => {
   const location = useLocation();
   const projectId = new URLSearchParams(location.search).get('projectId');
-  const frameService = useInterpret(createFrameMachine(projectId));
+  
+  const labelService = useInterpret(createLabelMachine(projectId));
+  labelService.start();
+
 
   return (
-    <FrameContext.Provider value={frameService}>
+    <LabelContext.Provider value={labelService}>
       {props.children}
-    </FrameContext.Provider>
+    </LabelContext.Provider>
   );
 };
+
+
 
 export default ServiceContext;
