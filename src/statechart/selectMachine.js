2fb5325da5f8dea78332c5c94d9dc86d44dcef90
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-03-29 16:49:09 -0700

    Add more selection actions

--- src/statechart/selectMachine.js
+++ src/statechart/selectMachine.js
@@ -10,6 +10,7 @@ const selectMachine = Machine(
       label: undefined,
       foreground: undefined,
       background: undefined,
+      newLabel: 10,
     },
     initial: 'idle',
     states: {
@@ -17,7 +18,16 @@ const selectMachine = Machine(
     },
     on: {
       LABELEDARRAY: { actions: ['updateLabeled', (context, event) => console.log(event)] },
-      COORDINATES: { actions: [ 'updateCoordinates', (context, event) => console.log(event)] },
+      COORDINATES: { actions: ['updateCoordinates', (context, event) => console.log(event)] },
+      SETFOREGROUND: { actions: 'setForeground' },
+      SETBACKGROUND: { actions: 'setBackground' },
+      'keydown.n': { actions: 'newForeground' },
+      'keydown.Escape': { actions: 'resetBackground' },
+      'keydown.x': { actions: 'swapLabels' },
+      'keydown.[': { actions: 'decrementForeground' },
+      'keydown.]': { actions: 'incrementForeground' },
+      'keydown.{': { actions: 'decrementBackground' },
+      'keydown.}': { actions: 'incrementBackground' },
     }
   },
   {
@@ -31,6 +41,26 @@ const selectMachine = Machine(
         y: event.y,
         label: context.labeledArray[event.y][event.x],
       })),
+      setForeground: assign({
+        foreground: (ctx, evt) => evt.label,
+        background: (ctx, evt) => evt.label === ctx.background ? ctx.foreground : ctx.background,
+      }),
+      setBackground: assign({
+        foreground: (ctx, evt) => evt.label === ctx.foreground ? ctx.background : ctx.foreground,
+        background: (ctx, evt) => evt.label,
+      }),
+      swapLabels: assign({
+        foreground: (ctx) => ctx.background,
+        background: (ctx) => ctx.foreground,
+      }),
+      newForeground: assign({ foreground: (ctx) => ctx.newLabel }),
+      newBackground: assign({ background: (ctx) => ctx.newLabel }),
+      resetForeground: assign({ foreground: 0 }),
+      resetBackground: assign({ background: 0 }),
+      incrementForeground: assign({ foreground: (ctx) => (ctx.foreground + 1) % ctx.newLabel }),
+      incrementBackground: assign({ background: (ctx) => (ctx.background + 1) % ctx.newLabel }),
+      decrementForeground: assign({ background: (ctx) => (ctx.foreground - 1 + ctx.newLabel) % ctx.newLabel }),
+      decrementBackground: assign({ background: (ctx) => (ctx.background + 1 + ctx.newLabel) % ctx.newLabel }),
     },
   },
 );
