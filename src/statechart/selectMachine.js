d30074356a740d5a3099f48127236b8a6f781228
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-04-01 11:40:42 -0700

    Add experimental changes to selectMachine Will likely be subsumed by toolMachine

--- src/statechart/selectMachine.js
+++ src/statechart/selectMachine.js
@@ -1,4 +1,4 @@
-import { Machine, actions, assign, forwardTo, send, sendParent } from 'xstate';
+import { Machine, actions, assign, forwardTo, send, sendParent, pure } from 'xstate';
 
 const selectMachine = Machine(
   {
@@ -11,27 +11,45 @@ const selectMachine = Machine(
       foreground: undefined,
       background: undefined,
       newLabel: 10,
+      subscribers: [],
     },
-    initial: 'idle',
+    initial: 'awaitLabeled',
     states: {
-      idle: {}
+      awaitLabeled: {
+        LABELEDARRAY: { actions: 'updateLabeled', target: 'send' },
+      },
+      idle: {
+        SETFOREGROUND: { actions: 'setForeground', target: 'sendSelected' },
+        SETBACKGROUND: { actions: 'setBackground', target: 'sendSelected' },
+        'keydown.n': { actions: 'newForeground', target: 'sendSelected' },
+        'keydown.Escape': { actions: 'resetBackground', target: 'sendSelected' },
+        'keydown.x': { actions: 'swapLabels', target: 'sendSelected' },
+        'keydown.[': { actions: 'decrementForeground', target: 'sendSelected' },
+        'keydown.]': { actions: 'incrementForeground', target: 'sendSelected' },
+        'keydown.{': { actions: 'decrementBackground', target: 'sendSelected' },
+        'keydown.}': { actions: 'incrementBackground', target: 'sendSelected' },
+      },
+      sendSelected: {
+        always: { actions: 'sendSelected', target: 'idle' }
+      },
     },
     on: {
-      LABELEDARRAY: { actions: ['updateLabeled', (context, event) => console.log(event)] },
-      COORDINATES: { actions: ['updateCoordinates', (context, event) => console.log(event)] },
-      SETFOREGROUND: { actions: 'setForeground' },
-      SETBACKGROUND: { actions: 'setBackground' },
-      'keydown.n': { actions: 'newForeground' },
-      'keydown.Escape': { actions: 'resetBackground' },
-      'keydown.x': { actions: 'swapLabels' },
-      'keydown.[': { actions: 'decrementForeground' },
-      'keydown.]': { actions: 'incrementForeground' },
-      'keydown.{': { actions: 'decrementBackground' },
-      'keydown.}': { actions: 'incrementBackground' },
+      SUBSCRIBE: { actions: 'addSubscriber' },
+      COORDINATES: { actions: 'updateCoordinates' },
+      LABELEDARRAY: { actions: 'updateLabeled' },
     }
   },
   {
     actions: {
+      addSubscriber: assign({
+        subscribers: (context, event) => [...context.subscribers, event.subscriber]
+      }),
+      sendSelected: pure((context, event) => context.subscribers.map(
+        (subscriber) => send(
+          { type: 'SELECTED', foreground: context.foreground, background: context.background, },
+          { to: subscriber }
+        )
+      )),
       updateLabeled: assign((context, event) => ({
         labeledArray: event.labeledArray,
         label: event.labeledArray[context.y][context.x],
@@ -42,12 +60,12 @@ const selectMachine = Machine(
         label: context.labeledArray[event.y][event.x],
       })),
       setForeground: assign({
-        foreground: (ctx, evt) => evt.label,
-        background: (ctx, evt) => evt.label === ctx.background ? ctx.foreground : ctx.background,
+        foreground: (ctx, evt) => ctx.label,
+        background: (ctx, evt) => ctx.label === ctx.background ? ctx.foreground : ctx.background,
       }),
       setBackground: assign({
-        foreground: (ctx, evt) => evt.label === ctx.foreground ? ctx.background : ctx.foreground,
-        background: (ctx, evt) => evt.label,
+        foreground: (ctx, evt) => ctx.label === ctx.foreground ? ctx.background : ctx.foreground,
+        background: (ctx, evt) => ctx.label,
       }),
       swapLabels: assign({
         foreground: (ctx) => ctx.background,
