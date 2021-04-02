520ad5324322f14bce20041211223709f558b71b
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-04-02 10:30:26 -0700

    Add trace to BrushCanvas

--- src/Canvas/BrushCanvas.js
+++ src/Canvas/BrushCanvas.js
@@ -4,11 +4,74 @@ import { useCanvas, useTool } from '../ServiceContext';
 const distance = (x, y) => {
   return Math.sqrt(Math.pow(y, 2) + Math.pow(x, 2));
 };
-const border = (x, y, cx, cy, radius) => {
-  return Math.floor(distance(x - cx, y - cy) + 1) === radius &&
+
+const onBrush = (x, y, brushSize) => {
+  const radius = brushSize - 1;
+  return Math.floor(distance(x - radius, y - radius)) === radius &&
     // not on border if next to border in both directions
-    !(Math.floor(distance(Math.abs(x - cx) + 1, y - cy) + 1) === radius && 
-      Math.floor(distance(x - cx, Math.abs(y - cy) + 1) + 1) === radius)
+    !(Math.floor(distance(Math.abs(x - radius) + 1, y - radius)) === radius && 
+      Math.floor(distance(x - radius, Math.abs(y - radius) + 1)) === radius)
+};
+
+/**
+ * Returns whether (x, y) is 
+ * @param {*} x 
+ * @param {*} y 
+ * @param {*} brushSize 
+ * @returns 
+ */
+const insideBrush = (x, y, brushSize) => {
+  const radius = brushSize - 1;
+  return Math.floor(distance(x - radius, y - radius)) <= radius;
+}
+
+/**
+ * Draws a a solid outline circle of radius brushSize on the context at (x, y).
+ * @param {*} ctx 
+ * @param {*} x 
+ * @param {*} y 
+ * @param {*} brushSize 
+ */
+const drawBrush = (ctx, x, y, brushSize) => {
+  const [sx, sy, sw, sh] = [x - brushSize + 1, y - brushSize + 1, 2 * brushSize - 1, 2 * brushSize - 1];
+  const imageData = ctx.getImageData(sx, sy, sw, sh);
+  const { data, height, width } = imageData;
+  for (let j = 0; j < height; j += 1) { // y
+    for (let i = 0; i < width; i += 1) { // x
+      if (onBrush(i, j, brushSize)) {
+        data[(j * width + i) * 4 + 0] = 255;
+        data[(j * width + i) * 4 + 1] = 255;
+        data[(j * width + i) * 4 + 2] = 255;
+        data[(j * width + i) * 4 + 3] = 255;
+      }
+    }
+  }
+  ctx.putImageData(imageData, sx, sy);
+}
+
+/**
+ * Draws a translucent, filled-in circle of radius brushSize on the context at (x, y).
+ * @param {*} ctx 
+ * @param {*} x 
+ * @param {*} y 
+ * @param {*} brushSize 
+ */
+const drawTrace = (ctx, x, y, brushSize) => {
+  const [sx, sy, sw, sh] = [x - brushSize + 1, y - brushSize + 1, 2 * brushSize - 1, 2 * brushSize - 1];
+  const imageData = ctx.getImageData(sx, sy, sw, sh);
+  const { data, height, width } = imageData;
+  const radius = brushSize - 1;
+  for (let j = 0; j < height; j += 1) { // y
+    for (let i = 0; i < width; i += 1) { // x
+      if (insideBrush(i, j, brushSize)) {
+        data[(j * width + i) * 4 + 0] = 255;
+        data[(j * width + i) * 4 + 1] = 255;
+        data[(j * width + i) * 4 + 2] = 255;
+        data[(j * width + i) * 4 + 3] = 255 / 2;
+      }
+    }
+  }
+  ctx.putImageData(imageData, sx, sy);
 };
 
 const BrushCanvas = props => {
@@ -20,55 +83,65 @@ const BrushCanvas = props => {
 
   const canvasRef = useRef();
   const ctx = useRef();
+
   useEffect(() => {
     ctx.current = canvasRef.current.getContext('2d');
     ctx.current.imageSmoothingEnabled = false;
-  }, [props]);
+  }, [props.height, props.width]);
 
-  // to convert the brush into an image
-  const brushCanvas = new OffscreenCanvas(width, height);
-  const brushCtx = brushCanvas.getContext('2d');
+  const brushCanvas = useRef();
+  const brushCtx = useRef();
+  const traceCanvas = useRef();
+  const traceCtx = useRef();
   useEffect(() => {
-    const array = new Uint8ClampedArray(width * height * 4);
-    for (let j = 0; j < height; j += 1) { // y
-      for (let i = 0; i < width; i += 1) { // x
-        if (border(i, j, x, y, brushSize)) {
-          array[(j * width  + i) * 4 + 0] = 255;
-          array[(j * width  + i) * 4 + 1] = 255;
-          array[(j * width  + i) * 4 + 2] = 255;
-          array[(j * width + i) * 4 + 3] = 255;
-        } else {
-          for (const [traceX, traceY] of trace) {
-            if (distance(i - traceX, j - traceY) < brushSize) {
-              array[(j * width  + i) * 4 + 0] = 255;
-              array[(j * width  + i) * 4 + 1] = 255;
-              array[(j * width  + i) * 4 + 2] = 255;
-              array[(j * width + i) * 4 + 3] = 255 / 2;
-            }
-          }
-        }
-      }
+    // holds brush outline
+    brushCanvas.current = new OffscreenCanvas(width, height);
+    brushCtx.current = brushCanvas.current.getContext('2d');
+    // // holds brush trace
+    traceCanvas.current = new OffscreenCanvas(width, height);
+    traceCtx.current = traceCanvas.current.getContext('2d');
+  }, [width, height]);
+
+
+
+  useEffect(() => {
+    brushCtx.current.clearRect(0, 0, height, width);
+    drawBrush(brushCtx.current, x, y, brushSize);
+  }, [brushCtx, x, y, brushSize, height, width]);
+
+  useEffect(() => {
+    if (trace.length === 0) { // clear the trace canvas
+      traceCtx.current.clearRect(0, 0, height, width);
+    } else { // add to trace
+      const [tx, ty] = trace[trace.length - 1];
+      drawTrace(traceCtx.current, tx, ty, brushSize);
     }
-    const data = new ImageData(array, width, height);
-    brushCtx.putImageData(data, 0, 0);
-  }, [brushCtx, x, y, trace, brushSize, height, width]);
-
-
-  // const traceCanvas = new OffscreenCanvas(width, height);
-  // const traceCtx = traceCanvas.getContext('2d');
+  }, [traceCtx, trace, brushSize, height, width]);
 
   useEffect(() => {
-    ctx.current.save();
+    traceCtx.current.clearRect(0, 0, height, width);
+    for (const [tx, ty] of trace) {
+      drawTrace(traceCtx.current, tx, ty, brushSize);
+    }
+  }, [traceCtx, brushSize]);
+
+  useEffect(() => {
     ctx.current.clearRect(0, 0, props.width, props.height);
     ctx.current.drawImage(
-      brushCanvas,
+      traceCanvas.current,
       sx, sy,
       width / zoom, height / zoom,
       0, 0,
       props.width, props.height,
     );
-    ctx.current.restore();
-  }, [brushCanvas, sx, sy, zoom, width, height, props.width, props.height]);
+    ctx.current.drawImage(
+      brushCanvas.current,
+      sx, sy,
+      width / zoom, height / zoom,
+      0, 0,
+      props.width, props.height,
+    );
+  }, [trace, brushSize, x, y, sx, sy, zoom, width, height, props.width, props.height]);
 
   return <canvas id='brush-canvas'
     ref={canvasRef}
