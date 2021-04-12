f57201d03cca84a657a667bc2d993ba8586210e7
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-04-12 16:00:22 -0400

    Style undo/redo buttons

--- src/ControlPanel/UndoRedo.js
+++ src/ControlPanel/UndoRedo.js
@@ -1,10 +1,17 @@
 
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import UndoIcon from '@material-ui/icons/Undo';
 import RedoIcon from '@material-ui/icons/Redo';
 
 import { useUndo } from '../ServiceContext';
+
+const useStyles = makeStyles({
+  button: {
+    width: '50%',
+  },
+});
 
 export default function UndoRedo() {
   const [state, send] = useUndo();
@@ -12,12 +19,26 @@ export default function UndoRedo() {
   const cannotUndo = action === 0;
   const cannotRedo = action === numActions;
 
+  const styles = useStyles();
+
   return <>
-    <Button variant="contained" color="primary" disabled={cannotUndo} onClick={() => send('UNDO')}>
+    <Button
+      className={styles.button}
+      variant="contained"
+      color="primary"
+      disabled={cannotUndo}
+      onClick={() => send('UNDO')}
+    >
       Undo
       <UndoIcon/>
     </Button >
-    <Button variant="contained" color="primary" disabled={cannotRedo} onClick={() => send('REDO')}>
+    <Button
+      className={styles.button}
+      variant="contained"
+      color="primary"
+      disabled={cannotRedo}
+      onClick={() => send('REDO')}
+    >
       Redo
       <RedoIcon/>
     </Button>
