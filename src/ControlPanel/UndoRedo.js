e31cd3e8d2c23ae69caf6400931ac683ec81202a
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-04-12 15:32:14 -0400

    Disable undo/redo buttons and add icons

--- src/ControlPanel/UndoRedo.js
+++ src/ControlPanel/UndoRedo.js
@@ -1,18 +1,25 @@
 
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import UndoIcon from '@material-ui/icons/Undo';
+import RedoIcon from '@material-ui/icons/Redo';
 
 import { useUndo } from '../ServiceContext';
 
 export default function UndoRedo() {
   const [state, send] = useUndo();
+  const { action, numActions } = state.context;
+  const cannotUndo = action === 0;
+  const cannotRedo = action === numActions;
 
   return <>
-    <Button variant="contained" color="primary" onClick={() => send('UNDO')}>
+    <Button variant="contained" color="primary" disabled={cannotUndo} onClick={() => send('UNDO')}>
       Undo
+      <UndoIcon/>
     </Button >
-    <Button variant="contained" color="primary" onClick={() => send('REDO')}>
+    <Button variant="contained" color="primary" disabled={cannotRedo} onClick={() => send('REDO')}>
       Redo
+      <RedoIcon/>
     </Button>
   </>;
 }
