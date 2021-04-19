b4589a38ef3baffa0a300803c033e9b944001424
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-04-19 13:41:17 -0400

    Use useSelector and split up Sliders

--- src/ControlPanel/ImageControls.js
+++ src/ControlPanel/ImageControls.js
@@ -1,42 +1,72 @@
 import React from 'react';
-import { useActor } from '@xstate/react';
+import { useSelector } from '@xstate/react';
 import ControlRow from './ControlRow';
 import DiscreteSlider from './DiscreteSlider';
 import { useImage } from '../ServiceContext';
 
 
-
-export default function ImageControls() {
+const FrameSlider = () => {
   const image = useImage();
-  const [current, send] = useActor(image);
-  const { frame, feature, channel, numFrames, numFeatures, numChannels } = current.context;
-
+  const frame = useSelector(image, state => state.context.frame);
+  const numFrames = useSelector(image, state => state.context.frame);
 
   const handleFrameChange = (event, newValue) => {
     if (newValue !== frame) {
-      send({ type: 'LOADFRAME', frame: newValue });
+      image.send({ type: 'LOADFRAME', frame: newValue });
     }
   };
 
+  return numFrames > 1 &&
+    <DiscreteSlider
+      label="Frame"
+      value={frame}
+      max={numFrames - 1}
+      onChange={handleFrameChange}
+    />;
+};
+
+const ChannelSlider = () => {
+  const image = useImage();
+  const channel = useSelector(image, state => state.context.channel);
+  const numChannels = useSelector(image, state => state.context.channel);
+
   const handleChannelChange = (event, newValue) => {
-    send({ type: 'LOADCHANNEL', channel: newValue });
+    image.send({ type: 'LOADCHANNEL', channel: newValue });
   };
+
+  return numChannels > 1 &&
+    <DiscreteSlider
+      label="Channel"
+      value={channel}
+      max={numChannels - 1}
+      onChange={handleChannelChange}
+    />;
+};
+
+const FeatureSlider = () => {
+  const image = useImage();
+  const feature = useSelector(image, state => state.context.feature);
+  const numFeatures = useSelector(image, state => state.context.feature);
 
   const handleFeatureChange = (event, newValue) => {
-    send({ type: 'LOADFEATURE', feature: newValue });
+    image.send({ type: 'LOADFEATURE', feature: newValue });
   };
 
+  return numFeatures > 1 &&
+    <DiscreteSlider
+      label="Feature"
+      value={feature}
+      max={numFeatures - 1}
+      onChange={handleFeatureChange}
+    />;
+};
+
+export default function ImageControls() {
   return (
     <ControlRow name={"Image"}>
-      { numFrames > 1 &&
-        <DiscreteSlider label="Frame" value={frame} max={numFrames - 1} onChange={handleFrameChange} />      
-      }
-      { numChannels > 1 &&
-        <DiscreteSlider label="Channel" value={channel} max={numChannels - 1} onChange={handleChannelChange}/>
-      }
-      { numFeatures > 1 &&
-        <DiscreteSlider label="Feature" value={feature} max={numFeatures - 1} onChange={handleFeatureChange} />
-      }
+      <FrameSlider />
+      <ChannelSlider />
+      <FeatureSlider />
     </ControlRow>
   )
 }
