857e360fbb1518d3ee9c96de1e35f7773ee82f5c
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-04-19 14:50:16 -0400

    Split up ChannelControls and use useSelector

--- src/ControlPanel/ChannelControls.js
+++ src/ControlPanel/ChannelControls.js
@@ -2,73 +2,98 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
 import ToggleButton from '@material-ui/lab/ToggleButton';
-import { useActor } from '@xstate/react';
+import { useSelector } from '@xstate/react';
 
 import ControlRow from './ControlRow';
 
+const InvertButton = ({ channel }) => {
+  const invert = useSelector(channel, state => state.context.invert);
 
-export default function ChannelControls({ channel }) {
-  const [current, send] = useActor(channel);
-  const { invert, grayscale, brightness, contrast } = current.context;
+  const handleInvertChange = (event) => {
+    channel.send({ type: 'TOGGLEINVERT' });
+  };
+
+  return <ToggleButton
+    value={invert}
+    selected={invert}
+    onChange={handleInvertChange}
+  >
+    Invert
+  </ToggleButton>;
+};
+
+const GrayscaleButton = ({ channel }) => {
+  const grayscale = useSelector(channel, state => state.context.grayscale);
+
+  const handleGrayscaleChange = (event) => {
+    channel.channelsend({ type: 'TOGGLEGRAYSCALE' });
+  };
+
+  return <ToggleButton
+    value={grayscale}
+    selected={grayscale}
+    onChange={handleGrayscaleChange}
+  >
+    Grayscale
+  </ToggleButton>;
+};
+
+const BrightnessSlider = ({ channel }) => {
+  const brightness = useSelector(channel, state => state.context.brightness);
 
   const handleBrightnessChange = (event, newValue) => {
     send({ type: 'SETBRIGHTNESS', brightness: newValue });
   };
 
+  return <>
+    <Typography gutterBottom>
+      Brightness
+    </Typography>
+    <Slider
+      value={brightness}
+      valueLabelDisplay="auto"
+      min={-1}
+      max={1}
+      step={0.01}
+      onChange={handleBrightnessChange}
+      onDoubleClick={() => handleBrightnessChange('', 0)}
+    />
+  </>;
+};
+
+const ContrastSlider = () => {
+  const contrast = useSelector(channel, state => state.context.contrast);
+
   const handleContrastChange = (event, newValue) => {
     send({ type: 'SETCONTRAST', contrast: newValue });
   };
 
-  const handleInvertChange = (event) => {
-    send({ type: 'TOGGLEINVERT' });
-  };
+  return <>
+    <Typography gutterBottom>
+      Contrast
+    </Typography>
+    <Slider
+      value={contrast}
+      valueLabelDisplay="auto"
+      min={-1}
+      max={1}
+      step={0.01}
+      onChange={handleContrastChange}
+      onDoubleClick={() => handleContrastChange('', 0)}
+    />
+  </>;
+};
 
-  const handleGrayscaleChange = (event) => {
-    send({ type: 'TOGGLEGRAYSCALE' });
-  };
 
+const ChannelControls = ({ channel }) => {
   return (
     <ControlRow name={"Raw Display"}>
-      <ToggleButton
-        value={invert}
-        selected={invert}
-        onChange={handleInvertChange}
-      >
-        Invert
-      </ToggleButton>
-
-      <ToggleButton
-        value={grayscale}
-        selected={grayscale}
-        onChange={handleGrayscaleChange}
-      >
-        Grayscale
-      </ToggleButton>
-      
-      <Typography gutterBottom>
-        Brightness
-      </Typography>
-      <Slider
-        value={brightness}
-        valueLabelDisplay="auto"
-        min={-1}
-        max={1}
-        step={0.01}
-        onChange={handleBrightnessChange}
-        onDoubleClick={() => handleBrightnessChange('', 0)}
-      />
-      <Typography gutterBottom>
-        Contrast
-      </Typography>
-      <Slider
-        value={contrast}
-        valueLabelDisplay="auto"
-        min={-1}
-        max={1}
-        step={0.01}
-        onChange={handleContrastChange}
-        onDoubleClick={() => handleContrastChange('', 0)}
-      />
+      <InvertButton channel={channel} />
+      <GrayscaleButton channel={channel} />
+      <BrightnessSlider channel={channel} />
+      <ContrastSlider channel={channel} />
     </ControlRow>
   );
 }
+
+export default React.memo(ChannelControls);
