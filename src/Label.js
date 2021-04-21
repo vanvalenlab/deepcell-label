d78ace1c92953bea94ad742f48391c2ecd38f879
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-04-21 09:10:57 -0400

    Fix whitespace

--- src/Label.js
+++ src/Label.js
@@ -7,7 +7,6 @@ import Canvas from './Canvas/Canvas';
 import InstructionPane from './InstructionPane';
 import Footer from './Footer/Footer';
 import { useState, useRef, useEffect } from 'react';
-
 import { useLocation } from "react-router-dom";
 
 const useStyles = makeStyles({
