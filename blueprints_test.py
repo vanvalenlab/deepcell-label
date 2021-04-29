318e9a51f78da049d15f8bd0ed440cbe7fddb79d
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-04-29 09:28:48 -0700

    Remove tests for old routes

--- blueprints_test.py
+++ blueprints_test.py
@@ -29,85 +29,26 @@ def test_health(client):
     assert response.json.get('message') == 'success'
 
 
-def test_change_display(client):
+def test_raw(client):
+    project = models.Project.create(DummyLoader())
+    response = client.get(f'/api/raw/{project.token}/0/0')
+    assert response.status_code == 200
 
-    response = client.post('/api/changedisplay/0/frame/999999')
-    assert response.status_code == 404
 
-    for filename in ('test.npz', 'test.trk'):
+def test_labeled(client):
+    project = models.Project.create(DummyLoader())
+    response = client.get(f'/api/labeled/{project.token}/0/0')
+    assert response.status_code == 200
 
-        # Create a project.
-        project = models.Project.create(DummyLoader(path=filename))
 
-        response = client.post('/api/changedisplay/{}/frame/0'.format(project.token))
-        # TODO: test correctness
-        assert 'raw' in response.json['imgs']
-        assert 'segmented' in response.json['imgs']
-        assert 'seg_arr' in response.json['imgs']
-
-        response = client.post('/api/changedisplay/{}/channel/0'.format(project.token))
-        assert 'raw' in response.json['imgs']
-        assert 'segmented' not in response.json['imgs']
-        assert 'seg_arr' not in response.json['imgs']
-
-        response = client.post('/api/changedisplay/{}/feature/0'.format(project.token))
-        assert 'raw' not in response.json['imgs']
-        assert 'segmented' in response.json['imgs']
-        assert 'seg_arr' in response.json['imgs']
-
-    # TODO: test handle error
+def test_array(client):
+    project = models.Project.create(DummyLoader())
+    response = client.get(f'/api/array/{project.token}/0/0')
+    assert response.status_code == 200
 
 
 def test_edit(client):
     pass
-
-
-def test_tool(client):
-    # test no form redirect
-    response = client.get('/tool')
-    assert response.status_code == 302
-
-    filename = 'test-file.npz'
-    response = client.post('/tool',
-                           content_type='multipart/form-data',
-                           data={'filename': filename})
-    assert response.status_code == 200
-    assert b'<body>' in response.data
-
-    filename = 'test-file.trk'
-    response = client.post('/tool',
-                           content_type='multipart/form-data',
-                           data={'filename': filename})
-    assert response.status_code == 200
-    assert b'<body>' in response.data
-
-    filename = 'test-file.badext'
-    response = client.post('/tool',
-                           content_type='multipart/form-data',
-                           data={'filename': filename})
-    assert response.status_code == 200
-
-
-def test_shortcut(client):
-    options = 'rgb=true&pixel_only=true&label_only=true'
-    response = client.get('/test-file.npz')
-    assert response.status_code == 200
-    assert b'<body>' in response.data
-
-    response = client.get('/test-file.npz?{}'.format(options))
-    assert response.status_code == 200
-    assert b'<body>' in response.data
-
-    response = client.get('/test-file.trk')
-    assert response.status_code == 200
-    assert b'<body>' in response.data
-
-    response = client.get('/test-file.trk?{}'.format(options))
-    assert response.status_code == 200
-    assert b'<body>' in response.data
-
-    response = client.get('/test-file.badext')
-    assert response.status_code == 200
 
 
 def test_undo(client):
@@ -137,7 +78,7 @@ def test_redo(client):
 
 
 def test_create_project(client, mocker):
-    mocker.patch('blueprints.loaders.get_loader', lambda *args: DummyLoader())
+    mocker.patch('blueprints.url_loaders.Loader', lambda *args: DummyLoader())
     response = client.post(f'/api/project')
     assert response.status_code == 200
 
@@ -148,50 +89,46 @@ def test_get_project(client):
     assert response.status_code == 200
 
 
-def test_project(client):
-    project = models.Project.create(DummyLoader())
-    response = client.get(f'/project/{project.token}')
-    assert response.status_code == 200
+
+def test_get_project_missing(client):
+    pass
+    # response = client.get('/project/abc')
+    # assert response.status_code == 404
 
 
-def test_project_missing(client):
-    response = client.get('/project/abc')
-    assert response.status_code == 404
+# def test_project_finished(client):
+#     project = models.Project.create(DummyLoader())
+#     project.finish()
+#     response = client.get(f'/project/{project.token}')
+#     assert response.status_code == 410
 
 
-def test_project_finished(client):
-    project = models.Project.create(DummyLoader())
-    project.finish()
-    response = client.get(f'/project/{project.token}')
-    assert response.status_code == 410
+# def test_download_project(client, mocker):
+#     project = models.Project.create(DummyLoader())
+#     mocked_export = mocker.patch('blueprints.exporters.Exporter.export',
+#                                  return_value=io.BytesIO())
+#     response = client.get(f'/downloadproject/{project.token}')
+#     assert response.status_code == 200
+#     mocked_export.assert_called()
 
 
-def test_download_project(client, mocker):
-    project = models.Project.create(DummyLoader())
-    mocked_export = mocker.patch('blueprints.exporters.Exporter.export',
-                                 return_value=io.BytesIO())
-    response = client.get(f'/downloadproject/{project.token}')
-    assert response.status_code == 200
-    mocked_export.assert_called()
+# def test_upload_to_s3_npz(client, mocker):
+#     project = models.Project.create(DummyLoader(path='test.npz'))
+#     mocked_export = mocker.patch('blueprints.exporters.S3Exporter.export')
+#     response = client.get(f'/api/upload/caliban-output/{project.token}')
+#     assert response.status_code == 302
+#     mocked_export.assert_called()
 
 
-def test_upload_to_s3_npz(client, mocker):
-    project = models.Project.create(DummyLoader(path='test.npz'))
-    mocked_export = mocker.patch('blueprints.exporters.S3Exporter.export')
-    response = client.get(f'/api/upload/caliban-output/{project.token}')
-    assert response.status_code == 302
-    mocked_export.assert_called()
+# def test_upload_to_s3_trk(client, mocker):
+#     project = models.Project.create(DummyLoader(path='test.trk'))
+#     mocked_export = mocker.patch('blueprints.exporters.S3Exporter.export')
+#     response = client.get(f'/api/upload/caliban-output/{project.token}')
+#     assert response.status_code == 302
+#     mocked_export.assert_called()
 
 
-def test_upload_to_s3_trk(client, mocker):
-    project = models.Project.create(DummyLoader(path='test.trk'))
-    mocked_export = mocker.patch('blueprints.exporters.S3Exporter.export')
-    response = client.get(f'/api/upload/caliban-output/{project.token}')
-    assert response.status_code == 302
-    mocked_export.assert_called()
-
-
-def test_upload_to_s3_missing(client, mocker):
-    mocker.patch('blueprints.exporters.S3Exporter.export')
-    response = client.get('/api/upload/caliban-output/1')
-    assert response.status_code == 404
+# def test_upload_to_s3_missing(client, mocker):
+#     mocker.patch('blueprints.exporters.S3Exporter.export')
+#     response = client.get('/api/upload/caliban-output/1')
+#     assert response.status_code == 404
