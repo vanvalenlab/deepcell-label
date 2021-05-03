7c336b2939e54d6216a3ccf0ebb51df16a5c702c
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-05-03 15:50:29 -0700

    Move python files into deepcell_label folder

---
+++ deepcell_label/url_loaders_test.py
@@ -0,0 +1,26 @@
+"""
+"""
+
+import io
+import pytest
+import numpy as np
+import responses
+
+import url_loaders
+
+# @responses.activate
+# def test_load_raw_npz():
+#     """Creates a dummy NPZ file with just a raw array and loads it."""
+#     expected = np.zeros((1, 1, 1, 1))
+#     npz = io.BytesIO()
+#     np.savez(npz, X=expected)
+#     npz.seek(0)
+#     url = 'http://example.com/mocked/raw.npz'
+#     responses.add(responses.GET, url, body=io.BufferedReader(npz))
+
+#     loader = url_loaders.Loader({'url': url})
+
+#     np.testing.assert_array_equal(loader.raw_array, expected)
+#     np.testing.assert_array_equal(loader.label_array, expected)
+#     assert loader.cell_ids is not None
+#     assert loader.cell_info is not None
