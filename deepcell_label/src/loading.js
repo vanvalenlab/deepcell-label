3b76002112e0caa8401280bdfba8520bd6bec55b
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-05-03 17:18:14 -0700

    Remove HTML, CSS, and JS for flask frontend

--- deepcell_label/src/loading.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('loading-bar').classList.add('active');
-const createProject = fetch(
-  `${document.location.origin}/api/project?source=s3&bucket=${inputBucket}&path=${path}`,
-  { method: 'POST' }
-).then(response => response.json());
-createProject.then((data) => { window.location = `/project/${data.projectId}?bucket=${outputBucket}`; });
