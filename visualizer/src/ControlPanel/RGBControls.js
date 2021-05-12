9000435f4e712836488180fa8bcd11d8b7b45a6c
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-05-11 17:29:45 -0700

    Update imports and remove unused components

--- visualizer/src/ControlPanel/RGBControls.js
+++ visualizer/src/ControlPanel/RGBControls.js
@@ -1,93 +1,11 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
-import ToggleButton from '@material-ui/lab/ToggleButton';
-import Radio from '@material-ui/core/Radio';
-import { withStyles } from '@material-ui/core/styles';
 import { useSelector } from '@xstate/react';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableCell from '@material-ui/core/TableCell';
-import TableRow from '@material-ui/core/TableRow';
 
 import ControlRow from './ControlRow';
-import { useChannel, useImage } from '../ServiceContext';
+import { useImage } from '../ServiceContext';
 
-const RedRadio = withStyles({
-  root: {
-    color: '#ff0000',
-    '&$checked': {
-      color: '#ff0000',
-    },
-  },
-  checked: {},
-})((props) => <Radio color="default" {...props} />);
-
-const GreenRadio = withStyles({
-  root: {
-    color: '#00ff00',
-    '&$checked': {
-      color: '#00ff00',
-    },
-  },
-  checked: {},
-})((props) => <Radio color="default" {...props} />);
-
-const BlueRadio = withStyles({
-  root: {
-    color: '#0000ff',
-    '&$checked': {
-      color: '#0000ff',
-    },
-  },
-  checked: {},
-})((props) => <Radio color="default" {...props} />);
-
-const ColorRadio = ({ channel }) => {
-  // const image = useImage();
-  // const channelColor = useSelector(channel, state => state.context.color);
-  const [channelColor, setChannelColor] = React.useState('red');
-
-  const handleChange = (event) => {
-    setChannelColor(event.target.value);
-  };
-
-  return <>
-    <RedRadio
-      checked={channelColor === 'red'}
-      onChange={handleChange}
-      value={'red'}
-      name="radio-button-demo"
-      inputProps={{ 'aria-label': 'red' }}
-    />
-    <GreenRadio
-      checked={channelColor === 'green'}
-      onChange={handleChange}
-      value={'green'}
-      name="radio-button-demo"
-      inputProps={{ 'aria-label': 'green' }}
-    />
-    <BlueRadio
-      checked={channelColor === 'blue'}
-      onChange={handleChange}
-      value={'blue'}
-      name="radio-button-demo"
-      inputProps={{ 'aria-label': 'blue' }}
-    />
-  </>;
-}
-
-const ChannelColorRow = ({ channel }) => {
-  console.log(channel);
-  const channelIndex = useSelector(channel, state => state.context.channel);
-  console.log(channelIndex);
-
-  return <>
-    <Typography>Channel {channelIndex}</Typography>
-    <ColorRadio channel={channel}/>
-  </>;
-}
 
 export const ChannelSlider = ({ channel, color }) => {
   const channelIndex = useSelector(channel, state => state.context.channel);
@@ -111,7 +29,6 @@ export const ChannelSlider = ({ channel, color }) => {
 }
 
 export const ChannelSliders = () => {
-
   const image = useImage();
   const channels = useSelector(image, state => state.context.channels);
   const colors = useSelector(image, state => state.context.channelColors);
@@ -124,19 +41,9 @@ export const ChannelSliders = () => {
 }
 
 const RGBControls = () => {
-  const image = useImage();
-  const channels = useSelector(image, state => state.context.channels);
-  const colors = useSelector(image, state => state.context.channelColors);
 
   return <ControlRow name={"RGB"}>
-    {/* <TableContainer size="small">
-      <Table >
-        <TableBody> */}
-    {/* {Object.values(channels).map((channel) => <ChannelColorRow channel={channel} />)} */}
     <ChannelSliders />
-    {/* </TableBody>
-      </Table>
-    </TableContainer> */}
   </ControlRow>;
 }
 
