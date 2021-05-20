ff0b5c8db5f3a4a5704cae410cf74f30664e90cd
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-05-20 12:24:03 -0700

    Add layer controls modified from Viv repo

---
+++ visualizer/src/ControlPanel/LayerController.js
@@ -0,0 +1,102 @@
+/** Modified from https://github.com/hms-dbmi/viv */
+import React from 'react';
+import { useSelector } from '@xstate/react';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import Slider from '@material-ui/core/Slider';
+import Select from '@material-ui/core/Select';
+import { makeStyles } from '@material-ui/core/styles';
+
+import ChannelOptions from './LayerOptions';
+import { useRaw } from '../ServiceContext';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    paddingTop: theme.spacing(1)
+  },
+  icon: {
+    color: theme.palette.text.primary,
+    marginTop: '4px'
+  }
+}));
+
+function LayerController({ layer }) {
+  const channel = useSelector(layer, state => state.context.channel);
+  const range = useSelector(layer, state => state.context.range);
+  const color = useSelector(layer, state => state.context.color);
+  const isOn = useSelector(layer, state => state.context.on);
+
+  const raw = useRaw();
+  const channelOptions = useSelector(raw, state => state.context.channelNames);
+  
+  const name = channelOptions[channel];
+  
+  const classes = useStyles();
+  const [min, max] = [0, 255];
+  const step = 1;
+
+  const handleSliderChange = (event, newValue) => {
+    layer.send({ type: 'SETRANGE', range: newValue });
+  };
+
+  return (
+    <Grid
+      container
+      direction="column"
+      m={2}
+      justify="center"
+      className={classes.root}
+    >
+      <Grid container direction="row" justify="space-between">
+        <Grid item xs={10}>
+          <Select
+            native
+            value={channel}
+            onChange={e => layer.send({ type: 'CHANGE_CHANNEL', channel: e.target.value })}
+          >
+            {channelOptions.map((opt, index) => (
+              <option key={index} value={index}>
+                {opt}
+              </option>
+            ))}
+          </Select>
+        </Grid>
+        <Grid item>
+          <ChannelOptions layer={layer} />
+        </Grid>
+      </Grid>
+      <Grid container direction="row" justify="flex-start" alignItems="center">
+        <Grid item xs={2}>
+          <Checkbox
+            onChange={() => layer.send('TOGGLE_ON')}
+            checked={isOn}
+            style={{
+              color: color,
+              '&$checked': {
+                color: color
+              }
+            }}
+          />
+        </Grid>
+        <Grid item xs={10}>
+          <Slider
+            value={range}
+            onChange={handleSliderChange}
+            valueLabelDisplay="off"
+            getAriaLabel={() => `${name}-${color}-${range}`}
+            min={min}
+            max={max}
+            step={step}
+            orientation="horizontal"
+            style={{
+              color: color,
+              marginTop: '7px'
+            }}
+          />
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default LayerController;
---
+++ visualizer/src/ControlPanel/LayerOptions.js
@@ -0,0 +1,80 @@
+/** Modified from https://github.com/hms-dbmi/viv */
+import React, { useReducer, useRef } from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+import ColorPalette from './ColorPalette';
+import { useRaw } from '../ServiceContext';
+
+const useStyles = makeStyles(() => ({
+  paper: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)'
+  },
+  span: {
+    width: '70px',
+    textAlign: 'center',
+    paddingLeft: '2px',
+    paddingRight: '2px',
+    color: 'white',
+  },
+  colors: {
+    '&:hover': {
+      backgroundColor: 'transparent'
+    },
+    paddingLeft: '2px',
+    paddingRight: '2px'
+  }
+}));
+
+function ChannelOptions({ layer }) {
+  const [open, toggle] = useReducer(v => !v, false);
+  const anchorRef = useRef(null);
+
+  const raw = useRaw();
+
+  const handleColorSelect = color => {
+    layer.send({ type: 'SETCOLOR', color });
+  };
+
+  const handleRemove = () => {
+    toggle();
+    raw.send({ type: 'REMOVE_LAYER', layer });
+  };
+
+  const classes = useStyles();
+  return (
+    <>
+      <IconButton
+        aria-label="Remove channel"
+        size="small"
+        onClick={toggle}
+        ref={anchorRef}
+      >
+        <MoreVertIcon fontSize="small" />
+      </IconButton>
+      <Popper open={open} anchorEl={anchorRef.current} placement="bottom-end">
+        <Paper className={classes.paper}>
+          <ClickAwayListener onClickAway={toggle}>
+            <MenuList id="channel-options">
+              <MenuItem dense disableGutters onClick={handleRemove}>
+                <span className={classes.span}>Remove</span>
+              </MenuItem>
+              <MenuItem dense disableGutters className={classes.colors}>
+                <ColorPalette handleChange={handleColorSelect} />
+              </MenuItem>
+            </MenuList>
+          </ClickAwayListener>
+        </Paper>
+      </Popper>
+    </>
+  );
+}
+
+export default ChannelOptions;
