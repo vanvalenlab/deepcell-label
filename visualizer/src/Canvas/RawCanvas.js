87cf5090dc3367b246467a1d7848496b3c3548c1
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-05-20 12:13:26 -0700

    Pass layer to ChannelCanvas and use renamed compose hook

--- visualizer/src/Canvas/RawCanvas.js
+++ visualizer/src/Canvas/RawCanvas.js
@@ -1,20 +1,15 @@
 import React, { useEffect, useRef } from 'react';
 import { useSelector } from '@xstate/react';
-import { useRaw, useComposeChannels } from '../ServiceContext';
+import { useRaw, useComposeLayers } from '../ServiceContext';
 import ChannelCanvas from './ChannelCanvas';
 
 export const RawCanvas = ({ sx, sy, sw, sh, zoom, width, height, className }) => {
   const raw = useRaw();
   const layers = useSelector(raw, state => state.context.layers);
-  const channels = useSelector(raw, state => state.context.channels);
-  const layerColors = useSelector(raw, state => state.context.layerColors);
-  const activeLayers = useSelector(raw, state => state.context.activeLayers);
-
-  console.log(layerColors);
 
   const canvasRef = useRef();
   const ctx = useRef();
-  const [composeCanvasRef, channelCanvases, setChannelCanvases] = useComposeChannels();
+  const [composeCanvasRef, canvases, setCanvases] = useComposeLayers();
 
   useEffect(() => {
     ctx.current = canvasRef.current.getContext('2d');
@@ -31,7 +26,7 @@ export const RawCanvas = ({ sx, sy, sw, sh, zoom, width, height, className }) =>
       0, 0,
       width, height,
     );
-  }, [composeCanvasRef, channelCanvases, sx, sy, zoom, sw, sh, width, height]);
+  }, [composeCanvasRef, canvases, sx, sy, zoom, sw, sh, width, height]);
 
   return <>
     {/* hidden processing canvas */}
@@ -49,14 +44,11 @@ export const RawCanvas = ({ sx, sy, sw, sh, zoom, width, height, className }) =>
       height={height}
     />
     {layers
-      .map((layer, index) => <ChannelCanvas
-        key={index}
-        channel={channels[layer]}
-        color={layerColors[index]}
-        setChannelCanvases={setChannelCanvases} />)
-      .filter((layer, index) =>
-        activeLayers[index]
-      )}
+      .map(layer => <ChannelCanvas
+        layer={layer}
+        setCanvases={setCanvases}
+      />)
+    }
   </>;
 };
 
