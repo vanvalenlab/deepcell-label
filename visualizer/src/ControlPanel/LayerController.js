268cb983024b94d3487d2fb39ea24c6d37f82d74
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-06-02 11:49:37 -0400

    Refactor LayerController to share between RGB and grayscale

--- visualizer/src/ControlPanel/LayerController.js
+++ visualizer/src/ControlPanel/LayerController.js
@@ -1,15 +1,17 @@
 /** Modified from https://github.com/hms-dbmi/viv */
 import React from 'react';
 import { useSelector } from '@xstate/react';
+import { makeStyles } from '@material-ui/core/styles';
 import Checkbox from '@material-ui/core/Checkbox';
 import Grid from '@material-ui/core/Grid';
 import Slider from '@material-ui/core/Slider';
 import Select from '@material-ui/core/Select';
-import ToggleButton from '@material-ui/lab/ToggleButton';
-import { makeStyles } from '@material-ui/core/styles';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import Switch from '@material-ui/core/Switch';
 
-import ChannelOptions from './LayerOptions';
-import { useRaw } from '../ServiceContext';
+import LayerOptions from './LayerOptions';
+import { useRaw, useImage } from '../ServiceContext';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -22,24 +24,10 @@ const useStyles = makeStyles(theme => ({
 }));
 
 function LayerController({ layer }) {
-  const channel = useSelector(layer, state => state.context.channel);
-  const range = useSelector(layer, state => state.context.range);
-  const color = useSelector(layer, state => state.context.color);
-  const isOn = useSelector(layer, state => state.context.on);
-
-  const raw = useRaw();
-  const invert = useSelector(raw, state => state.context.invert);
-  const channelOptions = useSelector(raw, state => state.context.channelNames);
-  
-  const name = channelOptions[channel];
-  
   const classes = useStyles();
-  const [min, max] = [0, 255];
-  const step = 1;
 
-  const handleSliderChange = (event, newValue) => {
-    layer.send({ type: 'SETRANGE', range: newValue });
-  };
+  const image = useImage();
+  const isRGB = useSelector(image, state => state.matches('color.color'));
 
   return (
     <Grid
@@ -50,55 +38,108 @@ function LayerController({ layer }) {
       className={classes.root}
     >
       <Grid container direction="row" justify="space-between">
-        <Grid item xs={10}>
-          <Select
-            native
-            value={channel}
-            onChange={e => layer.send({ type: 'CHANGE_CHANNEL', channel: e.target.value })}
-          >
-            {channelOptions.map((opt, index) => (
-              <option key={index} value={index}>
-                {opt}
-              </option>
-            ))}
-          </Select>
+        <Grid item xs={isRGB ? 10 : 8}>
+          <LayerSelector layer={layer} />
         </Grid>
         <Grid item>
-          <ChannelOptions layer={layer} />
+          {isRGB ? <LayerOptions layer={layer} /> : <InvertToggle />}
         </Grid>
       </Grid>
       <Grid container direction="row" justify="flex-start" alignItems="center">
         <Grid item xs={2}>
-          <Checkbox
-            onChange={() => layer.send('TOGGLE_ON')}
-            checked={isOn}
-            style={{
-              color: color,
-              '&$checked': {
-                color: color
-              }
-            }}
-          />
+          {isRGB && <LayerCheckbox layer={layer} />}
         </Grid>
         <Grid item xs={10}>
-          <Slider
-            value={range}
-            onChange={handleSliderChange}
-            valueLabelDisplay="off"
-            getAriaLabel={() => `${name}-${color}-${range}`}
-            min={min}
-            max={max}
-            step={step}
-            orientation="horizontal"
-            style={{
-              color: color,
-              marginTop: '7px'
-            }}
-          />
+          <LayerSlider layer={layer} />
         </Grid>
       </Grid>
     </Grid>
   );
 }
+
+const LayerSelector = ({ layer }) => {
+  const channel = useSelector(layer, state => state.context.channel);
+
+  const raw = useRaw();
+  const names = useSelector(raw, state => state.context.channelNames);
+
+  const onChange = e => {
+    layer.send({ type: 'CHANGE_CHANNEL', channel: e.target.value });
+  };
+  
+  return (
+    <Select
+      native
+      value={channel}
+      onChange={onChange}
+    >
+      {names.map((opt, index) => (
+        <option key={index} value={index}>
+          {opt}
+        </option>
+      ))}
+    </Select>
+  );
+}
+
+const LayerCheckbox = ({ layer }) => {
+  const color = useSelector(layer, state => state.context.color);
+  const isOn = useSelector(layer, state => state.context.on);
+
+  return <Checkbox
+    onChange={() => layer.send('TOGGLE_ON')}
+    checked={isOn}
+    style={{
+      color: color,
+      '&$checked': {
+        color: color
+      }
+    }}
+  />;
+};
+
+const LayerSlider = ({ layer }) => {
+  const range = useSelector(layer, state => state.context.range);
+  const color = useSelector(layer, state => state.context.color);
+
+  const image = useImage();
+  const isRGB = useSelector(image, state => state.matches('color.color'));
+
+  const onChange = (event, newValue) => {
+    layer.send({ type: 'SETRANGE', range: newValue });
+  };
+
+  return <Slider
+    value={range}
+    onChange={onChange}
+    valueLabelDisplay="off"
+    min={0}
+    max={255}
+    step={1}
+    orientation="horizontal"
+    style={{
+      color: isRGB ? color : 'primary',
+      marginTop: '7px'
+    }}
+  />;
+};
+
+const InvertToggle = () => {
+  const raw = useRaw();
+  const invert = useSelector(raw, state => state.context.invert);
+  
+  return (
+    <FormGroup row>
+      <FormControlLabel
+      control={<Switch
+        size='small' 
+        checked={invert} 
+        onChange={() => raw.send('TOGGLE_INVERT')} />}
+      label="Invert"
+      labelPlacement="start"
+    />
+    </FormGroup>
+  );
+};
 
 export default LayerController;
