5711d1761c9b6e2e76977ee982045ccf4d4fe639
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-06-02 11:51:03 -0400

    Add color mode toggle component

--- visualizer/src/ControlPanel/RawController.js
+++ visualizer/src/ControlPanel/RawController.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSelector } from '@xstate/react';
+import { useSelector, useActor } from '@xstate/react';
 import Tooltip from '@material-ui/core/Tooltip';
 import FormLabel from '@material-ui/core/FormLabel';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
@@ -8,11 +8,39 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import ToggleButton from '@material-ui/lab/ToggleButton';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import Switch from '@material-ui/core/Switch';
+import Typography from '@material-ui/core/Typography';
 
-import { useRaw } from '../ServiceContext';
+import { useRaw, useImage } from '../ServiceContext';
 import LayerController from './LayerController';
 
-export const MultiChannelController = () => {
+const ColorModeToggle = () => {
+  const image = useImage();
+  const [current, send] = useActor(image);
+
+  return (
+    <Typography component="div">
+        <Grid component="label" container alignItems="center" spacing={1}>
+          <Grid item align="right">Color <br /> (multi-channel)</Grid>
+          <Grid item>
+            <Switch 
+              color="default" 
+              checked={current.matches('color.grayscale')} 
+              onChange={() => send('TOGGLE_COLOR')} 
+            />
+          </Grid>
+          <Grid item>Grayscale <br /> (one channel)</Grid>
+        </Grid>
+      </Typography>
+  );
+};
+
+const RGBChannelController = () => {
   const raw = useRaw();
   const layers = useSelector(raw, state => state.context.layers);
   
@@ -40,7 +68,7 @@ export const MultiChannelController = () => {
   </>;
 }
 
-export const SingleChannelController = () => {
+const GrayscaleChannelController = () => {
   const raw = useRaw();
   const layer = useSelector(raw, state => state.context.layers[0]);
   
@@ -50,8 +78,8 @@ export const SingleChannelController = () => {
 }
 
 export const RawController = () => {
-  const raw = useRaw();
-  const invert = useSelector(raw, state => state.context.invert);
+  const image = useImage();
+  const [current] = useActor(image);
 
   return <>
     <Box display='flex' flexDirection='row' justifyContent='space-between'>
@@ -65,18 +93,9 @@ export const RawController = () => {
         <HelpOutlineIcon color="action" fontSize="large" />
       </Tooltip>
     </Box>
-    <MultiChannelController />
-    
-    <ToggleButton
-      onClick={() => raw.send('TOGGLE_INVERT')}
-      selected={invert}
-      fullWidth
-      variant="outlined"
-      // style={{ borderStyle: 'dashed' }}
-      size="medium"
-    >
-      Invert
-    </ToggleButton>
+    <ColorModeToggle />
+    { current.matches('color.color') && <RGBChannelController /> }
+    { current.matches('color.grayscale') && <GrayscaleChannelController />}
   </>;
 };
 
