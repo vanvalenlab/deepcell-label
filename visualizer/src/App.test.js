3906b4d1a42f6fa766b84260582d8520a9938d7a
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-07-06 14:28:07 -0700

    Update default test

--- visualizer/src/App.test.js
+++ visualizer/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders DeepCell Label', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/DeepCell Label/i);
   expect(linkElement).toBeInTheDocument();
 });
