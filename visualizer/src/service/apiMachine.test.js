8cc9f9460a121e7cc23c4ac0213503693b582d34
willgraf <7930703+willgraf@users.noreply.github.com>
2021-08-02 14:24:45 -0700

    Basic tests for XState and React components (#250)
    * Add basic tests for the Instructions component.
    
    * Update Instructions' wrapper Typography component.
    
    DOM validation failed when 'p' was a parent component of 'div'.
    
    * Fix App test to look for application name.
    
    * Basic tests for apiMachine.
    
    * Prefer single quotes.
    
    * Add @xstate/test and jest-fetch-mock as deps.
    
    * Simple test for syncToolState.
    
    * Update GitHub Actions with backend and frontend tests w/ coverage.
    
    * Fix whitespace
    
    * Move eslintrc into visualizer folder, remove eslint config from package.json.
    
    * Update path of coveralls command.
    
    * Don't check changed files.
    
    * working-direcotory -> path-to-lcov
    
    * Update test names for clarity.
    
    * variable in wrong scope.
    
    * Typo in lcov path.
    
    * Try using base-path for monorepo?
    
    * Add COVERALLS_REPO_TOKEN and fix flag-name
    
    * Comment out Finish Coveralls.
    
    The two coveralls reports are overwriting eachother.
    
    * Update deprecated -- in yarn test
    
    * Test multiple reports again.
    
    * Fix path to lcov
    
    * Merge tests into a single workflow to join the coverage reports.
    
    * Try base-path AND path-to-lcov
    
    * Add coverage badge to README.
    
    * Add Build Status badge
    
    * Lowercase workflow name and use it in the badge URL.
    
    * Actually fix badge URL.

---
+++ visualizer/src/service/apiMachine.test.js
@@ -0,0 +1,148 @@
+import { interpret } from 'xstate';
+import fetchMock from 'jest-fetch-mock';
+import createApiMachine from './apiMachine';
+
+fetchMock.enableMocks();
+
+describe('test apiMachine event handling', () => {
+
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
+
+  const backendEvents = [
+    'EDIT',
+    'BACKEND_UNDO',
+    'BACKEND_REDO',
+  ];
+
+  backendEvents.forEach((event) => {
+    it(`${event} event -> call API successfully -> "idle" state`, (done) => {
+      let edited = false;
+      let transitioned = false;
+      const apiMachine = createApiMachine({
+        projectId: 'testProjectId',
+        bucket: 'testBucket',
+      }).withConfig({
+        actions: {
+          sendEdited: () => {
+            edited = true
+          },
+        }
+      });
+      const apiService = interpret(apiMachine).onTransition((state) => {
+        // first event causes transition to loading
+        if (state.matches('loading')) {
+          expect(edited).not.toBeTruthy();
+          transitioned = true;
+        }
+        // after calling sendEdited the state will be idle
+        if (state.matches('idle') && transitioned) {
+          expect(edited).toBeTruthy();
+          done();
+        }
+      });
+
+      apiService.start();
+
+      // successful API call
+      fetch.mockResponseOnce(JSON.stringify({
+        data: {
+          frames: [],
+          labels: [],
+        }
+      }));
+      apiService.send({ type: event, args: [], action: 'test' });
+    });
+
+    it(`${event} event -> call API unsuccessfully -> "idle" state`, (done) => {
+      let failed = false;
+      let transitioned = false;
+      const apiMachine = createApiMachine({
+        projectId: 'testProjectId',
+        bucket: 'testBucket',
+      }).withConfig({
+        actions: {
+          sendError: () => {
+            failed = true
+          },
+        }
+      });
+      const apiService = interpret(apiMachine).onTransition((state) => {
+        // first event causes transition to loading
+        if (state.matches('loading')) {
+          expect(failed).not.toBeTruthy();
+          transitioned = true;
+        }
+        // after calling sendEdited the state will be idle
+        if (state.matches('idle') && transitioned) {
+          expect(failed).toBeTruthy();
+          done();
+        }
+      });
+
+      apiService.start();
+
+      // unsuccessful API call
+      fetch.mockResponseOnce(() => Promise.reject('API is down'));
+      apiService.send({ type: event, args: [], action: 'test' });
+    });
+  });
+
+  it('UPLOAD event -> call API successfully -> "idle" state', (done) => {
+    let transitioned = false;
+    const apiMachine = createApiMachine({
+      projectId: 'testProjectId',
+      bucket: 'testBucket',
+    });
+    const apiService = interpret(apiMachine).onTransition((state) => {
+      // first event causes transition to uploading.
+      if (state.matches('uploading')) {
+        transitioned = true;
+      }
+      // after calling sendEdited the state will be idle
+      if (state.matches('idle') && transitioned) {
+        done();
+      }
+    });
+
+    apiService.start();
+
+    // successful API call
+    fetch.mockResponseOnce(JSON.stringify({}));
+    apiService.send({ type: 'UPLOAD', args: [], action: 'test' });
+  });
+
+  it(`UPLOAD event -> call API unsuccessfully -> "idle" state`, (done) => {
+    let failed = false;
+    let transitioned = false;
+    const apiMachine = createApiMachine({
+      projectId: 'testProjectId',
+      bucket: 'testBucket',
+    }).withConfig({
+      actions: {
+        sendError: () => {
+          failed = true
+        },
+      }
+    });
+    const apiService = interpret(apiMachine).onTransition((state) => {
+      // first event causes transition to uploading
+      if (state.matches('uploading')) {
+        expect(failed).not.toBeTruthy();
+        transitioned = true;
+      }
+      // after calling sendEdited the state will be idle
+      if (state.matches('idle') && transitioned) {
+        expect(failed).toBeTruthy();
+        done();
+      }
+    });
+
+    apiService.start();
+
+    // unsuccessful API call
+    fetch.mockResponseOnce(() => Promise.reject('API is down'));
+    apiService.send({ type: 'UPLOAD', args: [], action: 'test' });
+  });
+});
