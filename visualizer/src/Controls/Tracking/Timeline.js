d53b56f069f8e5c104d1a165cfe8af74b9663c9d
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-08-16 13:45:56 -0700

    Display text with label under cursor

--- visualizer/src/Controls/Tracking/Timeline.js
+++ visualizer/src/Controls/Tracking/Timeline.js
@@ -21,27 +21,30 @@ function useDivision(label) {
 
 function Timeline() {
   const select = useSelect();
-  const foreground = useSelector(select, state => state.context.foreground);
-  const background = useSelector(select, state => state.context.background);
-  const label = foreground ? foreground : background;
+  const selected = useSelector(select, state => state.context.selected);
+  const label = useSelector(select, state => state.context.label);
 
   const tracking = useTracking();
   const addingDaughter = useSelector(tracking, state => state.matches('addingDaughter'));
-  const division = useSelector(tracking, state => state.context.labels[label]);
+  const division = useSelector(tracking, state => state.context.labels[selected]);
+  const parent = useSelector(tracking, state => state.context.parent);
 
   return (
     <Box>
-      <FrameSlider label={label} />
-      {label !== 0 && (
+      <FrameSlider label={selected} />
+      <Typography>Hovering over label: {label !== 0 && label}</Typography>
+      {selected !== 0 && (
         <Box style={{ display: 'flex', justifyContent: 'space-between' }}>
           {division?.parent && <Division label={division.parent} />}
           {/* Empty middle element keeps the second division on 
             the right side when the first is not present */}
           <Box></Box>
-          <Division label={label} />
+          <Division label={selected} />
         </Box>
       )}
-      {addingDaughter && <Typography>Click on a label to add it as a daughter.</Typography>}
+      {addingDaughter && (
+        <Typography>Click on a label to add a daughter to label {parent}.</Typography>
+      )}
     </Box>
   );
 }
