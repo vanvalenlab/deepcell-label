5a114e51327ad72c853a5b28e1c7aeb69ee001c6
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-09-03 14:00:52 -0700

    Add error boundary to alerts

--- visualizer/src/Controls/Tracking/Alerts/DivisionAlerts.js
+++ visualizer/src/Controls/Tracking/Alerts/DivisionAlerts.js
@@ -1,5 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import DaughterBeforeDivisionAlerts from './DaughterBeforeDivisionAlert';
 import OneDaughterAlerts from './OneDaughterAlerts';
 import ParentAfterDivisionAlerts from './ParentAfterDivisionAlert';
@@ -11,15 +12,32 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <div role='alert'>
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+}
+
 function DivisionAlerts() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <ParentAfterDivisionAlerts />
-      <DaughterBeforeDivisionAlerts />
-      <OneDaughterAlerts />
-    </div>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => {
+        // reset the state of your app so the error doesn't happen again
+      }}
+    >
+      <div className={classes.root}>
+        <ParentAfterDivisionAlerts />
+        <DaughterBeforeDivisionAlerts />
+        <OneDaughterAlerts />
+      </div>
+    </ErrorBoundary>
   );
 }
 
