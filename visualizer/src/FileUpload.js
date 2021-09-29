941202e5c3200109c18f0ca887ee9b6e1d97c7df
tddough98 <40068392+tddough98@users.noreply.github.com>
2021-09-29 11:49:42 -0700

    Add tooltip for axes form

--- visualizer/src/FileUpload.js
+++ visualizer/src/FileUpload.js
@@ -1,14 +1,15 @@
-// import HelpIcon from '@material-ui/icons/Help';
+import { Box } from '@material-ui/core';
 import Container from '@material-ui/core/Container';
 import FormControl from '@material-ui/core/FormControl';
-// import IconButton from '@material-ui/core/IconButton';
+import IconButton from '@material-ui/core/IconButton';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import { makeStyles } from '@material-ui/core/styles';
-// import Tooltip from '@material-ui/core/Tooltip';
+import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
 import CloudUpload from '@material-ui/icons/CloudUpload';
+import HelpIcon from '@material-ui/icons/Help';
 import axios from 'axios';
 import { PropTypes } from 'prop-types';
 import React, { useCallback, useState } from 'react';
@@ -40,29 +41,30 @@ function ImageAxesDropDown(props) {
   const allAxes = ['YXC', 'ZYXC', 'CYX', 'CZYX'];
 
   return (
-    <FormControl fullWidth>
-      <InputLabel id='image-axes-input-label'>Image Axes</InputLabel>
-      {/* TODO: use a tooltip to add more information. Can't get it to align */}
-      {/* <Tooltip title="What are the dimensions of the image?" placement='right'>
+    <Box display='flex'>
+      <FormControl fullWidth>
+        <InputLabel id='image-axes-input-label'>Dimension Order</InputLabel>
+        <Select
+          labelId='image-axes-select-label'
+          id='image-axes-select'
+          value={axes}
+          label='Axes'
+          onChange={onChange}
+          autoWidth
+        >
+          {allAxes.map((ax, i) => (
+            <MenuItem value={ax} key={i}>
+              {ax}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <Tooltip title='What are the dimensions of the image?' placement='right'>
         <IconButton>
           <HelpIcon />
         </IconButton>
-      </Tooltip> */}
-      <Select
-        labelId='image-axes-select-label'
-        id='image-axes-select'
-        value={axes}
-        label='Axes'
-        onChange={onChange}
-        autoWidth
-      >
-        {allAxes.map((ax, i) => (
-          <MenuItem value={ax} key={i}>
-            {ax}
-          </MenuItem>
-        ))}
-      </Select>
-    </FormControl>
+      </Tooltip>
+    </Box>
   );
 }
 
