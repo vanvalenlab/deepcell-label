8a9834915e6d87781780dc87b4545561e54be940
tddough98 <40068392+tddough98@users.noreply.github.com>
2022-01-13 13:48:07 -0800

    Use useDrawCanvas in other components

--- visualizer/src/Canvas/Raw/RGBCanvas.js
+++ visualizer/src/Canvas/Raw/RGBCanvas.js
@@ -1,42 +1,34 @@
 import { useSelector } from '@xstate/react';
-import React, { useEffect, useRef, useState } from 'react';
-import { useCanvas, useLayers } from '../../ProjectContext';
+import React, { useEffect, useState } from 'react';
+import { useCanvas, useDrawCanvas, useLayers } from '../../ProjectContext';
 import ChannelCanvas from './ChannelCanvas';
 
 export const RGBCanvas = ({ setCanvases }) => {
   const canvas = useCanvas();
-  const sw = useSelector(canvas, (state) => state.context.width);
-  const sh = useSelector(canvas, (state) => state.context.height);
+  const width = useSelector(canvas, (state) => state.context.width);
+  const height = useSelector(canvas, (state) => state.context.height);
 
   const layers = useLayers();
-
   // keys: layer index, values: ref to canvas for each layer
   const [layerCanvases, setLayerCanvases] = useState({});
-
-  const composeCanvasRef = useRef();
-  const composeCtxRef = useRef();
+  const canvasRef = useDrawCanvas();
 
   useEffect(() => {
-    composeCtxRef.current = composeCanvasRef.current.getContext('2d');
-    composeCtxRef.current.globalCompositeOperation = 'lighter';
-  }, [sh, sw]);
+    canvasRef.current.getContext('2d').globalCompositeOperation = 'lighter';
+  }, [canvasRef]);
 
   useEffect(() => {
-    composeCtxRef.current.clearRect(0, 0, sw, sh);
+    const ctx = canvasRef.current.getContext('2d');
+    ctx.clearRect(0, 0, width, height);
     for (let key in layerCanvases) {
-      composeCtxRef.current.drawImage(layerCanvases[key], 0, 0);
+      ctx.drawImage(layerCanvases[key], 0, 0);
     }
-    setCanvases((canvases) => ({ ...canvases, raw: composeCanvasRef.current }));
-  }, [layerCanvases, sh, sw, setCanvases]);
+    setCanvases((canvases) => ({ ...canvases, raw: canvasRef.current }));
+  }, [layerCanvases, width, height, setCanvases, canvasRef]);
 
-  return (
-    <>
-      <canvas id='compose-layers' hidden={true} ref={composeCanvasRef} width={sw} height={sh} />
-      {layers.map((layer) => (
-        <ChannelCanvas layer={layer} setCanvases={setLayerCanvases} key={layer.sessionId} />
-      ))}
-    </>
-  );
+  return layers.map((layer) => (
+    <ChannelCanvas layer={layer} setCanvases={setLayerCanvases} key={layer.sessionId} />
+  ));
 };
 
 export default RGBCanvas;
