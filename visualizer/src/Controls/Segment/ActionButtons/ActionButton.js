a2773e935857985553cb2b583c3974755048e959
tddough98 <40068392+tddough98@users.noreply.github.com>
2022-01-31 15:15:54 -0800

    Update withStyles to sx

--- visualizer/src/Controls/Segment/ActionButtons/ActionButton.js
+++ visualizer/src/Controls/Segment/ActionButtons/ActionButton.js
@@ -1,26 +1,16 @@
-import MuiButton from '@mui/material/Button';
+import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
-import withStyles from '@mui/styles/withStyles';
 import { bind } from 'mousetrap';
 import React, { useEffect } from 'react';
 
 // for adding tooltip to disabled buttons
 // from https://stackoverflow.com/questions/61115913
-const Button = withStyles({
-  root: {
-    padding: 4,
-    '&.Mui-disabled': {
-      pointerEvents: 'auto',
-    },
-  },
-})(MuiButton);
 
 const ActionButton = ({ tooltipText, disabled, onClick, hotkey, ...other }) => {
   const adjustedButtonProps = {
     disabled: disabled,
     component: disabled ? 'div' : undefined,
     onClick: disabled ? undefined : onClick,
-    sx: { padding: 0.5 },
   };
 
   useEffect(() => {
@@ -29,7 +19,17 @@ const ActionButton = ({ tooltipText, disabled, onClick, hotkey, ...other }) => {
 
   return (
     <Tooltip title={tooltipText}>
-      <Button {...other} {...adjustedButtonProps} />
+      <Button
+        {...other}
+        {...adjustedButtonProps}
+        classes='MuiButtonBase-root MuiToggleButton-root MuiToggleButton-sizeMedium MuiToggleButton-standard MuiToggleButtonGroup-grouped MuiToggleButtonGroup-groupedVertical'
+        sx={{
+          padding: 0.5,
+          '&.Mui-disabled': {
+            pointerEvents: 'auto',
+          },
+        }}
+      />
     </Tooltip>
   );
 };
--- visualizer/src/Controls/Segment/ToolButtons/ToolButton.js
+++ visualizer/src/Controls/Segment/ToolButtons/ToolButton.js
@@ -1,23 +1,13 @@
-import MuiToggleButton from '@mui/material/ToggleButton';
+import ToggleButton from '@mui/material/ToggleButton';
 import Tooltip from '@mui/material/Tooltip';
-import withStyles from '@mui/styles/withStyles';
 import { bind } from 'mousetrap';
 import React, { useEffect } from 'react';
 
 // for adding tooltip to disabled buttons
 // from https://stackoverflow.com/questions/61115913
-const ToggleButton = withStyles({
-  root: {
-    padding: 4,
-    '&.Mui-disabled': {
-      pointerEvents: 'auto',
-    },
-  },
-})(MuiToggleButton);
 
 function ToolButton(props) {
   const { hotkey, tooltipText, disabled, onClick, ...rest } = props;
-
   const adjustedButtonProps = {
     disabled: disabled,
     component: disabled ? 'div' : undefined,
@@ -30,7 +20,16 @@ function ToolButton(props) {
 
   return (
     <Tooltip title={tooltipText}>
-      <ToggleButton {...rest} {...adjustedButtonProps} />
+      <ToggleButton
+        {...rest}
+        {...adjustedButtonProps}
+        sx={{
+          padding: 0.5,
+          '&.Mui-disabled': {
+            pointerEvents: 'auto',
+          },
+        }}
+      />
     </Tooltip>
   );
 }
