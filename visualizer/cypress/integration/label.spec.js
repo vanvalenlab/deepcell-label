677968ace6e74a7f581e0d96ee7c7aa8c14cc04f
tddough98 <40068392+tddough98@users.noreply.github.com>
2022-02-24 13:15:40 -0800

    Add E2E test

---
+++ visualizer/cypress/integration/label.spec.js
@@ -0,0 +1,27 @@
+describe('First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true);
+  });
+});
+
+describe('Homepage content', () => {
+  it('finds DeepCell Label text', () => {
+    cy.visit('/');
+    cy.contains('DeepCell Label');
+    cy.contains('Upload');
+    cy.contains('select an example file');
+    cy.contains('Submit');
+    cy.contains('The Van Valen Lab');
+    cy.contains('Caltech');
+  });
+});
+
+describe('Create a project from an example file', () => {
+  it('disables uploading after submitting', () => {
+    cy.visit('/');
+    cy.get('.MuiNativeSelect-select').select('2D tissue segmentation');
+    cy.get('#submitExample').click();
+    cy.get('.MuiLinearProgress-root');
+    cy.url({ timeout: 30000 }).should('include', '/project');
+  });
+});
