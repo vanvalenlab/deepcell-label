035120c6edc9d14d7de8ad994f180a1cf44ba4cd
tddough98 <tddough98@users.noreply.github.com>
2022-03-21 11:57:21 -0700

    Add events to control spots display

--- visualizer/src/service/spotsMachine.js
+++ visualizer/src/service/spotsMachine.js
@@ -11,11 +11,20 @@ function createSpotsMachine() {
         spots: spots,
         opacity: 0.7,
         radius: 3, // radius in screen pixels
+        showSpots: true,
+      },
+      on: {
+        TOGGLE_SHOW_SPOTS: { actions: 'toggleShowSpots' },
+        SET_OPACITY: { actions: 'setOpacity' },
+        SET_RADIUS: { actions: 'setRadius' },
       },
     },
     {
       actions: {
         setSpots: assign({ labels: (ctx, evt) => evt.labels }),
+        toggleShowSpots: assign({ showSpots: (ctx, evt) => !ctx.showSpots }),
+        setOpacity: assign({ opacity: (ctx, evt) => evt.opacity }),
+        setRadius: assign({ radius: (ctx, evt) => evt.radius }),
       },
     }
   );
