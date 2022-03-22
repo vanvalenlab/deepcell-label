7061abd65b4f218875f207099d978f36c583987f
tddough98 <tddough98@users.noreply.github.com>
2022-03-22 08:09:10 -0700

    Split spot control components into separate files

--- visualizer/src/Controls/SpotsControls/SpotsControls.js
+++ visualizer/src/Controls/SpotsControls/SpotsControls.js
@@ -1,56 +1,8 @@
 import { FormLabel, Typography } from '@mui/material';
-import Checkbox from '@mui/material/Checkbox';
 import Grid from '@mui/material/Grid';
-import Slider from '@mui/material/Slider';
-import { useSelector } from '@xstate/react';
-import { useCanvas, useSpots } from '../../ProjectContext';
-
-function SpotsCheckbox() {
-  const spots = useSpots();
-  const isOn = useSelector(spots, (state) => state.context.showSpots);
-
-  return <Checkbox sx={{ p: 0 }} onChange={() => spots.send('TOGGLE_SHOW_SPOTS')} checked={isOn} />;
-}
-
-function SpotOpacitySlider() {
-  const spots = useSpots();
-  const opacity = useSelector(spots, (state) => state.context.opacity);
-  return (
-    <Slider
-      value={opacity}
-      onChange={(_, value) => spots.send({ type: 'SET_OPACITY', opacity: value })}
-      min={0}
-      max={1}
-      step={0.01}
-      orientation='horizontal'
-      valueLabelDisplay='auto'
-    />
-  );
-}
-
-function SpotRadiusSlider() {
-  const canvas = useCanvas();
-  const height = useSelector(canvas, (state) => state.context.height);
-  const width = useSelector(canvas, (state) => state.context.width);
-  const scale = useSelector(canvas, (state) => state.context.scale);
-
-  const maxRadius = Math.floor(0.01 * Math.min(height, width) * window.devicePixelRatio * scale);
-
-  const spots = useSpots();
-  const radius = useSelector(spots, (state) => state.context.radius);
-
-  return (
-    <Slider
-      value={radius}
-      onChange={(_, value) => spots.send({ type: 'SET_RADIUS', radius: value })}
-      min={0}
-      max={maxRadius}
-      step={1}
-      orientation='horizontal'
-      valueLabelDisplay='auto'
-    />
-  );
-}
+import SpotOpacitySlider from './SpotOpacitySlider';
+import SpotRadiusSlider from './SpotRadiusSlider';
+import SpotsCheckbox from './SpotsCheckbox';
 
 function SpotsControls() {
   return (
