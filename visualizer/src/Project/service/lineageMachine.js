156b7c0a6be4dcfb2e3bd8eeb38dfecb70363a61
tddough98 <tddough98@users.noreply.github.com>
2022-05-02 13:18:58 -0700

    Add selected label

--- visualizer/src/Project/service/lineageMachine.js
+++ visualizer/src/Project/service/lineageMachine.js
@@ -3,28 +3,65 @@
 import { assign, Machine } from 'xstate';
 import { fromEventBus } from './eventBus';
 
+function previousCell(cell, lineage) {
+  const all = Object.keys(lineage).map(Number);
+  const smaller = all.filter((c) => c < cell);
+  const previous = Math.max(...smaller);
+  if (previous === -Infinity) {
+    return Math.max(...all);
+  }
+  return previous;
+}
+
+function nextCell(cell, lineage) {
+  const all = Object.keys(lineage).map(Number);
+  const larger = all.filter((c) => c > cell);
+  const next = Math.min(...larger);
+  if (next === Infinity) {
+    return Math.min(...all);
+  }
+  return next;
+}
+
 function createLineageMachine({ eventBuses }) {
   return Machine(
     {
-      id: 'spots',
-      invoke: [{ src: fromEventBus('labeled', () => eventBuses.load) }],
-      context: {
-        lineage: null,
+      context: { lineage: null, selected: 0, hovering: 0 },
+      invoke: [
+        { src: fromEventBus('lineage', () => eventBuses.load) },
+        { src: fromEventBus('lineage', () => eventBuses.canvas) },
+      ],
+      id: 'lineage',
+      on: {
+        HOVERING: { actions: 'setHovering' },
       },
-      on: {},
       initial: 'loading',
       states: {
         loading: {
           on: {
-            LOADED: { target: 'loaded', actions: 'setLineage' },
+            LOADED: { actions: 'setLineage', target: 'loaded' },
           },
         },
-        loaded: {},
+        loaded: {
+          on: {
+            mouseup: { actions: 'selectCell' },
+            SET_CELL: { actions: 'setCell' },
+            RESET_CELL: { actions: 'resetCell' },
+            NEXT_CELL: { actions: 'selectNextCell' },
+            PREV_CELL: { actions: 'selectPreviousCell' },
+          },
+        },
       },
     },
     {
       actions: {
-        setLineage: assign({ spots: (ctx, evt) => evt.lineage }),
+        setHovering: assign({ hovering: (ctx, evt) => evt.hovering }),
+        selectCell: assign({ selected: (ctx, evt) => ctx.hovering }),
+        setLineage: assign({ lineage: (ctx, evt) => evt.lineage }),
+        setCell: assign({ selected: (ctx, evt) => evt.cell }),
+        resetCell: assign({ selected: 0 }),
+        selectNextCell: assign({ selected: (ctx) => nextCell(ctx.selected, ctx.lineage) }),
+        selectPreviousCell: assign({ selected: (ctx) => previousCell(ctx.selected, ctx.lineage) }),
       },
     }
   );
