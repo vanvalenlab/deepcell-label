d7f6ed20986625ee27385cb9c3258976e668c7f4
tddough98 <tddough98@users.noreply.github.com>
2022-05-13 11:25:35 -0700

    Switch from endwith('trk') to is_tarfile

--- deepcell_label/loaders.py
+++ deepcell_label/loaders.py
@@ -159,7 +159,7 @@ def load_segmentation(f):
         if y is None:
             y = load_zip_tiffs(zf, filename='y.ome.tiff')
         return y
-    if hasattr(f, 'filename') and f.filename.endswith('.trk'):
+    if tarfile.is_tarfile(f):
         return load_trk(f, filename='tracked.npy')
 
 
@@ -194,7 +194,7 @@ def load_lineage(f):
     if zipfile.is_zipfile(f):
         zf = zipfile.ZipFile(f, 'r')
         lineage = load_zip_json(zf, filename='lineage.json')
-    elif hasattr(f, 'filename') and f.filename.endswith('.trk'):
+    elif tarfile.is_tarfile(f):
         lineage = load_trk(f, filename='lineage.json')
     if lineage is not None:
         lineage = reformat_lineage(lineage)
@@ -435,7 +435,7 @@ def load_png(f):
 
 def load_trk(f, filename='raw.npy'):
     f.seek(0)
-    if hasattr(f, 'filename') and f.filename.endswith('.trk'):
+    if tarfile.is_tarfile(f):
         with tarfile.open(fileobj=f) as trks:
             if filename == 'raw.npy' or filename == 'tracked.npy':
                 # numpy can't read these from disk...
