40f64edc825a635e67111fae1618afb813c7d881
tddough98 <tddough98@users.noreply.github.com>
2022-05-16 11:31:27 -0700

    Introduce DummyEdit to bypass loading from zip

--- deepcell_label/label_test.py
+++ deepcell_label/label_test.py
@@ -12,6 +12,33 @@ def enable_transactional_tests(app, db_session):
     db_session.autoflush = False
 
 
+# Bypasses loading data from a zip file
+class DummyEdit(Edit):
+    def __init__(
+        self,
+        labels=None,
+        overlaps=None,
+        action=None,
+        args=None,
+        raw=None,
+        lineage=None,
+        write_mode='overlap',
+    ):
+        self.labels = labels
+        self.overlaps = overlaps
+        self.new_value = self.overlaps.shape[0]
+        self.new_label = self.overlaps.shape[1]
+        self.action = action
+        self.args = args
+        self.raw = raw
+        self.lineage = lineage
+        self.write_mode = write_mode
+        super().__init__(labels_zip=None)
+
+    def load(self, labels_zip):
+        pass
+
+
 class TestEdit:
     def test_action_flood_background(self, app):
         """Flooding background does no spread to diagonal areas."""
@@ -31,10 +58,11 @@ class TestEdit:
             [0, 1, 0],
             [0, 0, 1]])
         # fmt: on
-        edit = Edit(labels, overlaps=overlaps)
+        action = 'flood'
+        args = {'foreground': 2, 'background': 0, 'x': 1, 'y': 1}
 
         with app.app_context():
-            edit.action_flood(2, 0, 1, 1)
+            edit = DummyEdit(labels=labels, overlaps=overlaps, action=action, args=args)
             np.testing.assert_array_equal(edit.labels, expected_labels)
 
     def test_action_flood_label(self, app):
@@ -56,10 +84,14 @@ class TestEdit:
             [0, 1, 0],
             [0, 0, 1]])
         # fmt: on
-        edit = Edit(labels, overlaps=overlaps)
 
         with app.app_context():
-            edit.action_flood(2, 1, 1, 0)
+            edit = DummyEdit(
+                labels=labels,
+                overlaps=overlaps,
+                action='flood',
+                args={'foreground': 2, 'background': 1, 'x': 1, 'y': 0},
+            )
             np.testing.assert_array_equal(edit.labels, expected_labels)
 
     def test_action_draw_remove_label(self, app):
@@ -67,10 +99,14 @@ class TestEdit:
         labels = np.array([[1]], dtype=np.int32)
         expected = np.array([[0]], dtype=np.int32)
         overlaps = np.array([[0, 0], [0, 1]])
-        edit = Edit(labels, overlaps=overlaps)
 
         with app.app_context():
-            edit.action_draw([(0, 0)], 1, 1, True)
+            edit = DummyEdit(
+                labels=labels,
+                overlaps=overlaps,
+                action='draw',
+                args={'trace': '[[0, 0]]', 'brush_size': 1, 'label': 1, 'erase': True},
+            )
             np.testing.assert_array_equal(edit.labels, expected)
 
     def test_action_handle_draw_add_label(self, app):
@@ -78,20 +114,28 @@ class TestEdit:
         labels = np.array([[0]], dtype=np.int32)
         expected = np.array([[1]], dtype=np.int32)
         overlaps = np.array([[0, 0], [0, 1]])
-        edit = Edit(labels, overlaps=overlaps)
 
         with app.app_context():
-            edit.action_draw([(0, 0)], 1, 1, False)
+            edit = DummyEdit(
+                labels=labels,
+                overlaps=overlaps,
+                action='draw',
+                args={'trace': '[[0, 0]]', 'brush_size': 1, 'label': 1, 'erase': False},
+            )
             np.testing.assert_array_equal(edit.labels, expected)
 
     def test_action_replace(self, app):
         labels = np.array([[1, 1], [2, 2]], dtype=np.int32)
         expected = np.array([[1, 1], [1, 1]], dtype=np.int32)
         overlaps = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 1]])
-        edit = Edit(labels, overlaps=overlaps)
 
         with app.app_context():
-            edit.action_replace(1, 2)
+            edit = DummyEdit(
+                labels=labels,
+                overlaps=overlaps,
+                action='replace',
+                args={'a': 1, 'b': 2},
+            )
             np.testing.assert_array_equal(edit.labels, expected)
 
     def test_action_active_contour_other_labels_unchanged(self, app):
@@ -104,10 +148,15 @@ class TestEdit:
         initial_labels = labels.copy()
         raw = np.identity(10)
         overlaps = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 1]])
-        edit = Edit(labels, raw, overlaps=overlaps)
 
         with app.app_context():
-            edit.action_active_contour(1)
+            edit = DummyEdit(
+                labels=labels,
+                overlaps=overlaps,
+                action='active_contour',
+                args={'label': 1},
+                raw=raw,
+            )
             np.testing.assert_array_equal(initial_labels == 2, edit.labels == 2)
 
     # TODO: active contouring has no effect in this test; find out what correct behavior is
@@ -138,10 +187,15 @@ class TestEdit:
         labels[-1, :] = 0
         initial_labels = labels.copy()
         overlaps = np.array([[0, 0], [0, 1]])
-        edit = Edit(labels, raw, overlaps=overlaps)
 
         with app.app_context():
-            edit.action_active_contour(1, min_pixels=1)
+            edit = DummyEdit(
+                labels=labels,
+                overlaps=overlaps,
+                action='active_contour',
+                args={'label': 1, 'min_pixels': 1},
+                raw=raw,
+            )
             assert int((edit.labels == 1).sum()) < int((initial_labels == 1).sum())
 
     def test_action_erode_delete_label(self, app):
@@ -149,10 +203,14 @@ class TestEdit:
         labels = np.zeros((3, 3), dtype=np.int32)
         labels[1, 1] = 1
         overlaps = np.array([[0, 0], [0, 1]])
-        edit = Edit(labels, overlaps=overlaps)
 
         with app.app_context():
-            edit.action_erode(1)
+            edit = DummyEdit(
+                labels=labels,
+                overlaps=overlaps,
+                action='erode',
+                args={'label': 1},
+            )
             assert 1 not in edit.labels
 
     def test_action_erode_other_labels_unchanged(self, app):
@@ -160,43 +218,62 @@ class TestEdit:
         labels = np.array([[1, 1], [2, 2]], dtype=np.int32)
         initial_labels = labels.copy()
         overlaps = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 1]])
-        edit = Edit(labels, overlaps=overlaps)
 
         with app.app_context():
-            edit.action_erode(1)
+            edit = DummyEdit(
+                labels=labels,
+                overlaps=overlaps,
+                action='erode',
+                args={'label': 1},
+            )
             np.testing.assert_array_equal(initial_labels == 2, edit.labels == 2)
 
     def test_action_dilate_overlapping(self, app):
-        """Tests dilating a label when write_mode is overlap."""
+        """Dilating a label creates a new overlap value when write_mode is overlap."""
         labels = np.array([[1, 1], [2, 2]], dtype=np.int32)
         expected_labels = np.array([[1, 1], [3, 3]], dtype=np.int32)
         overlaps = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 1]])
         expected_overlaps = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 1]])
-        edit = Edit(labels, overlaps=overlaps, write_mode='overlap')
 
         with app.app_context():
-            edit.action_dilate(1)
+            edit = DummyEdit(
+                labels=labels,
+                overlaps=overlaps,
+                action='dilate',
+                args={'label': 1},
+                write_mode='overlap',
+            )
             np.testing.assert_array_equal(edit.labels, expected_labels)
             np.testing.assert_array_equal(edit.overlaps, expected_overlaps)
 
     def test_action_dilate_overwrite(self, app):
-        """Tests that a dilated label does not affect other labels when write_mode is exclude."""
+        """Dilating a label removes other labels when write_mode is overwrite."""
         labels = np.array([[1, 1], [2, 2]], dtype=np.int32)
         expected_labels = np.array([[1, 1], [1, 1]], dtype=np.int32)
         overlaps = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 1]])
-        edit = Edit(labels, overlaps=overlaps, write_mode='overwrite')
 
         with app.app_context():
-            edit.action_dilate(1)
-            np.testing.assert_array_equal(labels, expected_labels)
+            edit = DummyEdit(
+                labels=labels,
+                overlaps=overlaps,
+                action='dilate',
+                args={'label': 1},
+                write_mode='overwrite',
+            )
+            np.testing.assert_array_equal(edit.labels, expected_labels)
 
     def test_action_dilate_exclude(self, app):
-        """Tests that a dilated label does not affect other labels when write_mode is exclude."""
+        """Filated label does not affect other labels when write_mode is exclude."""
         labels = np.array([[1, 1], [2, 2]], dtype=np.int32)
         expected_labels = labels.copy()
         overlaps = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 1]])
-        edit = Edit(labels, overlaps=overlaps, write_mode='exclude')
 
         with app.app_context():
-            edit.action_dilate(1)
+            edit = DummyEdit(
+                labels=labels,
+                overlaps=overlaps,
+                action='dilate',
+                args={'label': 1},
+                write_mode='exclude',
+            )
             np.testing.assert_array_equal(expected_labels, edit.labels)
