ab6785ead7249e2d0fa69abb34be1477f9473500
tddough98 <tddough98@users.noreply.github.com>
2022-05-19 14:05:14 -0700

    Use Overlaps class

--- visualizer/src/Project/service/overlapsMachine.js
+++ visualizer/src/Project/service/overlapsMachine.js
@@ -1,6 +1,8 @@
 /** Loads and stores overlaps arrays. */
 
+import colormap from 'colormap';
 import { assign, Machine, send } from 'xstate';
+import Overlaps from '../overlaps';
 import { fromEventBus } from './eventBus';
 
 const createOverlapsMachine = ({ eventBuses }) =>
@@ -11,24 +13,37 @@ const createOverlapsMachine = ({ eventBuses }) =>
         { id: 'eventBus', src: fromEventBus('overlaps', () => eventBuses.overlaps) },
         { src: fromEventBus('overlaps', () => eventBuses.api) },
         { src: fromEventBus('overlaps', () => eventBuses.load) },
+        { src: fromEventBus('overlaps', () => eventBuses.image) },
       ],
       context: {
-        overlaps: null,
+        overlaps: null, // Overlaps object
+        frame: 0,
+        colormap: [
+          [0, 0, 0, 1],
+          ...colormap({ colormap: 'viridis', format: 'rgba' }),
+          [255, 255, 255, 1],
+        ],
       },
       initial: 'waiting',
+      on: {
+        FRAME: { actions: 'setFrame' },
+      },
       states: {
         waiting: {
           on: {
             LOADED: {
               target: 'idle',
-              actions: 'setOverlaps',
+              actions: ['setOverlaps', 'setColormap'],
             },
           },
         },
         idle: {
-          entry: 'sendOverlaps',
+          entry: ['sendOverlaps', 'sendOverlapMatrix'],
           on: {
-            EDITED: { actions: ['setOverlaps', 'sendOverlaps'] },
+            FRAME: { actions: ['setFrame', 'sendOverlapMatrix'] },
+            EDITED: {
+              actions: ['updateOverlaps', 'setColormap', 'sendOverlaps', 'sendOverlapMatrix'],
+            },
           },
         },
       },
@@ -36,7 +51,17 @@ const createOverlapsMachine = ({ eventBuses }) =>
     {
       guards: {},
       actions: {
-        setOverlaps: assign({ overlaps: (ctx, evt) => evt.overlaps }),
+        setFrame: assign({ frame: (_, event) => event.frame }),
+        setOverlaps: assign({ overlaps: (_, event) => event.overlaps }),
+        updateOverlaps: assign({
+          overlaps: (ctx, evt) => {
+            console.log(ctx, evt);
+            return new Overlaps([
+              ...ctx.overlaps.overlaps.filter((o) => o.z !== evt.frame),
+              ...evt.overlaps.map((o) => ({ ...o, z: evt.frame })),
+            ]);
+          },
+        }),
         sendOverlaps: send(
           (ctx, evt) => ({
             type: 'OVERLAPS',
@@ -44,6 +69,24 @@ const createOverlapsMachine = ({ eventBuses }) =>
           }),
           { to: 'eventBus' }
         ),
+        sendOverlapMatrix: send(
+          (ctx, evt) => ({
+            type: 'OVERLAP_MATRIX',
+            overlapMatrix: ctx.overlaps.getMatrix(ctx.frame),
+          }),
+          { to: 'eventBus' }
+        ),
+        setColormap: assign({
+          colormap: (ctx, evt) => [
+            [0, 0, 0, 1],
+            ...colormap({
+              colormap: 'viridis',
+              nshades: Math.max(9, ctx.overlaps.getNewCell() - 1),
+              format: 'rgba',
+            }),
+            [255, 255, 255, 1],
+          ],
+        }),
       },
     }
   );
