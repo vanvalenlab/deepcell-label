01c399f1df8c66de331036226301cb89aa01b112
tddough98 <tddough98@users.noreply.github.com>
2022-05-27 10:34:11 -0700

    Prevent changing machine for useInterpret

--- visualizer/src/Project/Load.js
+++ visualizer/src/Project/Load.js
@@ -1,10 +1,12 @@
 import { useInterpret, useSelector } from '@xstate/react';
+import { useState } from 'react';
 import Project from './Project';
 import ProjectContext from './ProjectContext';
 import createProjectMachine from './service/projectMachine';
 
-function Load({ id, spots }) {
-  const project = useInterpret(createProjectMachine(id));
+function Load({ id }) {
+  const [projectMachine] = useState(createProjectMachine(id));
+  const project = useInterpret(projectMachine);
   window.dcl = project;
   const track = useSelector(project, (state) => state.context.track);
 
