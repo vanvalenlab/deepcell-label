252360cfd1bb06cfbf275b2a0abc1231a5a25b59
tddough98 <tddough98@users.noreply.github.com>
2022-05-27 13:12:05 -0700

    Fix saving actor instead of tool name

--- visualizer/src/Project/service/edit/segment/editSegmentMachine.js
+++ visualizer/src/Project/service/edit/segment/editSegmentMachine.js
@@ -107,7 +107,7 @@ const createEditSegmentMachine = (context) =>
           send('EXIT', { to: ctx.tools[ctx.tool] }),
           assign({ tool: evt.tool }),
         ]),
-        save: respond((ctx) => ({ type: 'RESTORE', tool: ctx.tools[ctx.tool] })),
+        save: respond((ctx) => ({ type: 'RESTORE', tool: ctx.tool })),
         restore: assign({ tool: (_, evt) => evt.tool }),
         spawnTools: assign({
           tools: (context) => ({
