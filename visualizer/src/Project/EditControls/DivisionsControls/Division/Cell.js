eaf2648c1c4585f0252872210ac23e5b1a1526b4
tddough98 <tddough98@users.noreply.github.com>
2022-06-03 13:04:35 -0700

    Add contrast text color change to cells in division UI

--- visualizer/src/Project/EditControls/DivisionsControls/Division/Cell.js
+++ visualizer/src/Project/EditControls/DivisionsControls/Division/Cell.js
@@ -1,19 +1,24 @@
 import Avatar from '@mui/material/Avatar';
 import React from 'react';
+import { contrast } from '../../../DisplayControls/Cells/Selected/utils';
 import { useHexColormap } from '../../../ProjectContext';
 
 const Cell = React.forwardRef(({ cell, onClick }, ref) => {
   const colors = useHexColormap();
   const color = colors[cell] ?? '#000000';
-
-  if (!cell) {
-    return null;
-  }
+  const textColor = contrast(color, '#000000') > contrast(color, '#FFFFFF') ? '#000000' : '#FFFFFF';
 
   return (
     <Avatar
       ref={ref}
-      sx={{ my: 0.25, mx: 1, height: '2.5rem', width: '2.5rem', backgroundColor: color }}
+      sx={{
+        my: 0.25,
+        mx: 1,
+        height: '2.5rem',
+        width: '2.5rem',
+        backgroundColor: color,
+        color: textColor,
+      }}
       onClick={onClick}
     >
       {cell}
