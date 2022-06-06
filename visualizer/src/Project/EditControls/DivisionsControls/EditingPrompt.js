376abe32e794e823b4e237ce7400eab336e8f830
tddough98 <tddough98@users.noreply.github.com>
2022-06-06 11:03:46 -0700

    Update add daughter prompt text with two stages

--- visualizer/src/Project/EditControls/DivisionsControls/EditingPrompt.js
+++ visualizer/src/Project/EditControls/DivisionsControls/EditingPrompt.js
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { useSelector } from '@xstate/react';
 import React from 'react';
 import { useEditDivisions } from '../../ProjectContext';
@@ -6,16 +6,23 @@ import { useEditDivisions } from '../../ProjectContext';
 function EditingPrompt() {
   const editDivisions = useEditDivisions();
   const addingDaughter = useSelector(editDivisions, (state) => state.matches('addingDaughter'));
-  const parent = useSelector(editDivisions, (state) => state.context.parent);
+  const parent = useSelector(editDivisions, (state) => state.context.selected);
+  const daughter = useSelector(editDivisions, (state) => state.context.daughter);
 
   if (!addingDaughter) {
     return null;
   }
 
   return (
-    <Typography sx={{ maxWidth: '100%' }}>
-      Click a label to add a daughter to label {parent}.
-    </Typography>
+    <Box sx={{ maxWidth: '142px' }}>
+      {daughter ? (
+        <Typography>
+          Click again to add daughter {daughter} to cell {parent}.
+        </Typography>
+      ) : (
+        <Typography>Click a cell to select the daughter.</Typography>
+      )}
+    </Box>
   );
 }
 
