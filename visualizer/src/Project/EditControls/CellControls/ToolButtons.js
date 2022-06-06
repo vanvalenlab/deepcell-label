1e3b7bd81a16bceaca6d4a1b11192c5859e35e2c
tddough98 <tddough98@users.noreply.github.com>
2022-06-06 11:37:06 -0700

    Factor out onClick for ToggleButtonGroup

--- visualizer/src/Project/EditControls/CellControls/ToolButtons.js
+++ visualizer/src/Project/EditControls/CellControls/ToolButtons.js
@@ -2,50 +2,37 @@ import { FormLabel, ToggleButton } from '@mui/material';
 import Box from '@mui/material/Box';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import { useSelector } from '@xstate/react';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useEditCells } from '../../ProjectContext';
 
 function ToolButtons() {
   const editCells = useEditCells();
   const tool = useSelector(editCells, (state) => state.context.tool);
 
+  const onClick = useCallback(
+    (event, value) => {
+      editCells.send({ type: 'SET_TOOL', tool: value });
+    },
+    [editCells]
+  );
+
   return (
     <Box display='flex' flexDirection='column'>
       <FormLabel>Tools</FormLabel>
-      <ToggleButtonGroup orientation='vertical' value={tool}>
-        <ToggleButton
-          onClick={() => editCells.send({ type: 'SET_TOOL', tool: 'select' })}
-          value={'select'}
-          sx={{ px: 0.5, py: 0 }}
-        >
+      <ToggleButtonGroup orientation='vertical' value={tool} onClick={onClick}>
+        <ToggleButton value={'select'} sx={{ px: 0.5, py: 0 }}>
           Select
         </ToggleButton>
-        <ToggleButton
-          onClick={() => editCells.send({ type: 'SET_TOOL', tool: 'delete' })}
-          value={'delete'}
-          sx={{ px: 0.5, py: 0 }}
-        >
+        <ToggleButton value={'delete'} sx={{ px: 0.5, py: 0 }}>
           Delete
         </ToggleButton>
-        <ToggleButton
-          onClick={() => editCells.send({ type: 'SET_TOOL', tool: 'replace' })}
-          value={'replace'}
-          sx={{ px: 0.5, py: 0 }}
-        >
+        <ToggleButton value={'replace'} sx={{ px: 0.5, py: 0 }}>
           Replace
         </ToggleButton>
-        <ToggleButton
-          onClick={() => editCells.send({ type: 'SET_TOOL', tool: 'swap' })}
-          value={'swap'}
-          sx={{ px: 0.5, py: 0 }}
-        >
+        <ToggleButton value={'swap'} sx={{ px: 0.5, py: 0 }}>
           Swap
         </ToggleButton>
-        <ToggleButton
-          onClick={() => editCells.send({ type: 'SET_TOOL', tool: 'new' })}
-          value={'new'}
-          sx={{ px: 0.5, py: 0 }}
-        >
+        <ToggleButton value={'new'} sx={{ px: 0.5, py: 0 }}>
           New
         </ToggleButton>
       </ToggleButtonGroup>
