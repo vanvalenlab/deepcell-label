449e0efa47a6bf6427eae000d0c61ac7da7baca3
tddough98 <tddough98@users.noreply.github.com>
2022-06-08 14:18:57 -0700

    Switch == to ===

--- visualizer/src/Project/DisplayControls/TimeControls/TimeBox.js
+++ visualizer/src/Project/DisplayControls/TimeControls/TimeBox.js
@@ -9,8 +9,8 @@ function TimeBox({ t, duration, color }) {
     position: 'absolute',
     backgroundColor: color,
     height: '0.5rem',
-    width: t == 0 || t == duration - 1 ? `calc(${width / 2}% + 1px)` : `calc(${width}% + 1px)`,
-    left: t == 0 ? 0 : `${width * (t - 0.5)}%`,
+    width: t === 0 || t === duration - 1 ? `calc(${width / 2}% + 1px)` : `calc(${width}% + 1px)`,
+    left: t === 0 ? 0 : `${width * (t - 0.5)}%`,
   };
 
   const onClick = (event, newValue) => {
