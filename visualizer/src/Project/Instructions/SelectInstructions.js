cbcbca7b691b4c65b62251dcafe925a1f2b7132a
tddough98 <tddough98@users.noreply.github.com>
2022-06-09 17:08:00 -0700

    Update SelectInstructions

--- visualizer/src/Project/Instructions/SelectInstructions.js
+++ visualizer/src/Project/Instructions/SelectInstructions.js
@@ -1,14 +1,19 @@
-import { Box, Grid } from '@mui/material';
+import { Box, FormLabel, Grid } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import React from 'react';
+import Hovering from '../DisplayControls/Cells/Hovering';
+import NewCellButton from '../DisplayControls/Cells/Selected/NewCellButton';
+import NextCellButton from '../DisplayControls/Cells/Selected/NextCellButton';
+import PreviousCellButton from '../DisplayControls/Cells/Selected/PreviousCellButton';
+import ResetCellButton from '../DisplayControls/Cells/Selected/ResetCellButton';
 import Selected from '../DisplayControls/Cells/Selected/Selected';
 import { Shortcut, Shortcuts } from './Shortcuts';
 
 function SelectShortcuts() {
   return (
     <Shortcuts>
-      <Shortcut text='New label' shortcut='N' />
-      <Shortcut text='Unselect ' shortcut='Esc' />
+      <Shortcut text='Select new label' shortcut='N' />
+      <Shortcut text='Reset selected label ' shortcut='Esc' />
       <Shortcut text='Select previous label' shortcut='[' />
       <Shortcut text='Select next label' shortcut=']' />
     </Shortcuts>
@@ -19,24 +24,27 @@ function SelectInstructions() {
   return (
     <Box display='flex' justifyContent='space-between'>
       <div>
-        <Typography variant='h5'>Selected Label Palette</Typography>
         <Grid container spacing={3}>
-          <Grid item xs={6} sm={4} md={2}>
-            <Selected />
-          </Grid>
-          <Grid item xs={6} sm={8} md={10}>
+          <Grid item xs={12}>
             <Typography>
-              This box shows the selected label and has controls to cycle, reset, or select a new
-              label.
+              <Box display='inline-block'>
+                <Selected />
+              </Box>{' '}
+              shows the selected cell. <PreviousCellButton /> and <NextCellButton /> cycle the
+              selected cell, <NewCellButton />
+              selects a new cell, and <ResetCellButton /> resets the selected cell.
             </Typography>
           </Grid>
+          <Grid item xs={2} display='flex' justifyContent='center'>
+            <Box display='flex' flexDirection='column'>
+              <FormLabel>Hovering</FormLabel>
+              <Hovering />
+            </Box>
+          </Grid>
+          <Grid item xs={10}>
+            <Typography>Hover over a cell on the canvas to see what cell it is.</Typography>
+          </Grid>
         </Grid>
-        <Typography variant='h5'>Selecting Labels</Typography>
-        <Typography>
-          Click on labels on the canvas to select them.
-          <br />
-          While using the Select tool, click to select a label.
-        </Typography>
       </div>
       <SelectShortcuts />
     </Box>
