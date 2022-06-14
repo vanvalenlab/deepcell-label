b9b7dcd3dbbfab3a69799c833f0de15fdfdb0fae
tddough98 <tddough98@users.noreply.github.com>
2022-06-14 12:17:50 -0700

    Make list

--- visualizer/src/Project/Instructions/OverviewInstructions.js
+++ visualizer/src/Project/Instructions/OverviewInstructions.js
@@ -4,9 +4,13 @@ import React from 'react';
 function OverviewInstructions() {
   return (
     <Typography>
-      DeepCell Label annotates biological images by segmenting cells, assigning cells over time, and
-      capturing cell divisions. Labeled data are shown on the canvas on the right, and controls are
-      on the left.
+      DeepCell Label annotates biological images by
+      <ul>
+        <li>segmenting cells</li>
+        <li>assigning cells over time</li>
+        <li>capturing cell divisions</li>
+      </ul>
+      Labeled data are shown on the canvas on the right, and controls are on the left.
       <br />
       On the far left, there is an export button, tabs to edit each label type, and controls to
       display the labeled data. The tabs change which controls are shown in the next column to the
