7c0ccd33014e71e5eed507c6b0077233511932bf
tddough98 <tddough98@users.noreply.github.com>
2022-06-21 16:18:31 -0700

    Address performance when moving frames

--- visualizer/src/Project/service/idbMachine.js
+++ visualizer/src/Project/service/idbMachine.js
@@ -1,8 +1,9 @@
 /** Records the current project state in IDB so that it can be restored when closing.
  * MVP only records the labeled data state, not the UI state or the undo/redo history.
+ * Converts from cell list to Cells object.
  */
 import IdbWorker from 'worker-loader!./idbWebWorker'; // eslint-disable-line import/no-webpack-loader-syntax
-import { assign, Machine, send, sendParent } from 'xstate';
+import { assign, forwardTo, Machine, send, sendParent } from 'xstate';
 import Cells from '../cells';
 import { fromEventBus } from './eventBus';
 import { fromWebWorker } from './from-web-worker';
@@ -12,16 +13,13 @@ function createIDBMachine({ projectId, eventBuses }) {
     {
       context: {
         projectId,
-        project: {
-          raw: null, // Uint8Array[][][]
-          labeled: null, // Int32Array[][][]
-          cells: null, // list of cells, not the Cells object
-          divisions: null, // list of divisions
-        },
+        loadedEvent: null, // hold onto LOADED event while prompting overwrite
       },
       invoke: [
         { src: fromWebWorker(() => new IdbWorker()), id: 'idb' },
-        { src: fromEventBus('IDB', () => eventBuses.arrays, 'LABELED') },
+        {
+          src: fromEventBus('IDB', () => eventBuses.arrays, ['EDITED_SEGMENT', 'RESTORED_SEGMENT']),
+        },
         { src: fromEventBus('IDB', () => eventBuses.cells, 'CELLS') },
         { src: fromEventBus('IDB', () => eventBuses.divisions, 'DIVISIONS') },
         { src: fromEventBus('IDB', () => eventBuses.spots, 'SPOTS') },
@@ -35,11 +33,11 @@ function createIDBMachine({ projectId, eventBuses }) {
             LOADED: [
               {
                 cond: 'forceLoadOutput',
-                actions: ['setProject', 'setLoaded'],
+                actions: 'setLoaded',
                 target: 'promptForceLoadOutput',
               },
               {
-                actions: ['setProject', 'setLoaded', 'sendLoaded'],
+                actions: ['setLoaded', 'sendLoaded'],
                 target: 'idle',
               },
             ],
@@ -64,17 +62,20 @@ function createIDBMachine({ projectId, eventBuses }) {
         loadProject: {
           on: {
             LOADED: {
-              actions: ['setProject', 'putProject'],
+              actions: send((ctx, evt) => ({ ...evt, cells: evt.cells.cells }), { to: 'idb' }),
               target: 'idle',
             },
           },
         },
         idle: {
           on: {
-            LABELED: { actions: ['updateLabeled', 'putProject'] },
-            CELLS: { actions: ['updateCells', 'putProject'] },
-            DIVISIONS: { actions: ['updateDivisions', 'putProject'] },
-            SPOTS: { actions: ['updateSpots', 'putProject'] },
+            EDITED_SEGMENT: { actions: forwardTo('idb') },
+            RESTORED_SEGMENT: { actions: forwardTo('idb') },
+            CELLS: {
+              actions: send((ctx, evt) => ({ ...evt, cells: evt.cells.cells }), { to: 'idb' }),
+            },
+            DIVISIONS: { actions: forwardTo('idb') },
+            SPOTS: { actions: forwardTo('idb') },
           },
         },
       },
@@ -88,42 +89,11 @@ function createIDBMachine({ projectId, eventBuses }) {
         getProject: send((ctx) => ({ type: 'PROJECT_ID', projectId: ctx.projectId }), {
           to: 'idb',
         }),
-        updateLabeled: assign({
-          project: (ctx, evt) => {
-            const { t, feature } = evt;
-            const labeled = ctx.project.labeled.map((arr, i) =>
-              i === feature ? arr.map((arr, j) => (j === t ? evt.labeled : arr)) : arr
-            );
-            return { ...ctx.project, labeled };
-          },
-        }),
-        updateCells: assign({
-          project: (ctx, evt) => ({ ...ctx.project, cells: evt.cells.cells }),
-        }),
-        updateDivisions: assign({
-          project: (ctx, evt) => ({ ...ctx.project, divisions: evt.divisions }),
-        }),
-        updateSpots: assign({
-          project: (ctx, evt) => ({ ...ctx.project, spots: evt.spots }),
-        }),
-        setProject: assign((ctx, evt) => ({
-          project: {
-            raw: evt.raw,
-            labeled: evt.labeled,
-            cells: evt.cells.cells, // LOADED sends Cells object, need to get cells list
-            divisions: evt.divisions,
-            spots: evt.spots,
-          },
-        })),
         setLoaded: assign({
-          loadedEvent: (ctx, evt) => ({ ...evt, cells: new Cells(evt.cells) }),
+          loaded: (ctx, evt) => ({ ...evt, cells: new Cells(evt.cells) }),
         }),
-        sendLoaded: sendParent((ctx) => ctx.loadedEvent),
+        sendLoaded: sendParent((ctx) => ctx.loaded),
         forwardToParent: sendParent((ctx, evt) => evt),
-        putProject: send(
-          (ctx) => ({ type: 'PUT_PROJECT', projectId: ctx.projectId, project: ctx.project }),
-          { to: 'idb' }
-        ),
       },
     }
   );
