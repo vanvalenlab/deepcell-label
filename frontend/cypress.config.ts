f8f2f8da181d5500946373ad28d62d5a88eb916b
ykevu <kyu3@caltech.edu>
2023-02-03 17:33:59 -0800

    Change cypress.json to cypress.config.ts

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   video: false,
+  env: {
+    AWS_ACCESS_KEY_ID: '',
+    AWS_SECRET_ACCESS_KEY: '',
+  },
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
