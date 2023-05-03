70cc84ca4543d41a1e6bc7b377831ba5bec82b55
ykevu <kyu3@caltech.edu>
2023-05-03 12:52:45 -0700

    Update marker panel for consistency with master channel list

--- frontend/src/Project/EditControls/CellTypeControls/CellTypeUI/CellMarkerPanel.js
+++ frontend/src/Project/EditControls/CellTypeControls/CellTypeUI/CellMarkerPanel.js
@@ -1,16 +1,16 @@
 export const markerPanel = [
   { id: 0, names: ['B cell', 'B', 'BCELL'], channels: ['CD19', 'CD20'] },
-  { id: 1, names: ['dendritic cell', 'DC', 'dendritic'], channels: ['CD11c', 'CSF1R'] },
-  { id: 2, names: ['endothelial'], channels: ['CD31', 'VIM'] },
+  { id: 1, names: ['dendritic cell', 'DC', 'dendritic'], channels: ['CD11C', 'CSF1R'] },
+  { id: 2, names: ['endothelial'], channels: ['CD31', 'VIMENTIN'] },
   { id: 3, names: ['epithelial'], channels: ['ECADHERIN'] },
-  { id: 4, names: ['fibroblast'], channels: ['FAP', 'VIM'] },
+  { id: 4, names: ['fibroblast'], channels: ['FAP', 'VIMENTIN'] },
   {
     id: 5,
     names: ['macrophage', 'macro', 'macs'],
     channels: ['CD11B', 'CD68', 'CD14', 'CD163', 'CD16', 'CSF1R'],
   },
-  { id: 6, names: ['mast cell', 'mast'], channels: ['Tryptase'] },
-  { id: 7, names: ['monocyte', 'mono'], channels: ['CD11b', 'CSF1R'] },
+  { id: 6, names: ['mast cell', 'mast'], channels: ['TRYPTASE'] },
+  { id: 7, names: ['monocyte', 'mono'], channels: ['CD11B', 'CSF1R'] },
   {
     id: 8,
     names: ['natural killer cell', 'NK'],
@@ -22,5 +22,5 @@ export const markerPanel = [
     names: ['T cell', 'T', 'T regulatory', 'Treg', 'TCELL/TREG'],
     channels: ['CD3', 'CD4', 'CD8', 'FOXP3', 'CD103', 'ICOS', 'LAG3/CD223'],
   },
-  { id: 11, names: ['tumor'], channels: ['PanCK', 'GALECTIN9', 'HER2'] },
+  { id: 11, names: ['tumor'], channels: ['PANCK', 'GALECTIN9', 'HER2'] },
 ];
